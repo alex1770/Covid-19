4d40117aec7708668035cf9239969278e88e78bf
Alex Selby <alex.selby@pobox.com>
2020-05-23 00:10:35 +0100

    Adjusted trafficlevel format slightly

--- parsetraffic.py
+++ parsetraffic.py
@@ -19,23 +19,24 @@ try:
     for x in fp:
       if x[:1]!='#':
         y=x.strip().split()
-        levels[y[0]]=[int(z) for z in y[1:]]
+        levels[(y[0],y[1])]=[int(z) for z in y[2:]]
 except FileNotFoundError:
   pass
 
 for fn in sorted(listdir(tdir)):
-  if fn[:6]=='London' and fn[-4:]=='.png' and fn[:-4] not in levels:
-    im_frame = Image.open(join(tdir,fn))
-    np_frame = np.array(im_frame)
-    im_frame.close()
-    rgb = np_frame[:,:,0]*65536+np_frame[:,:,1]*256+np_frame[:,:,2]
-    numcols = [np.sum(rgb==v) for v in [whitecol,greencol,orangecol,redcol,darkredcol]]
-    levels[fn[:-4]]=numcols
-    print("Read",fn)
-    #traffic = numcols[2]+3*numcols[3]+10*numcols[4]
-    #print(fn,"%8d"%traffic)
+  if fn[:6]=='London' and fn[-4:]=='.png':
+    f=fn[:-4].find('.')
+    key=(fn[:f],fn[f+1:-4])
+    if key not in levels:
+      im_frame = Image.open(join(tdir,fn))
+      np_frame = np.array(im_frame)
+      im_frame.close()
+      rgb = np_frame[:,:,0]*65536+np_frame[:,:,1]*256+np_frame[:,:,2]
+      numcols = [np.sum(rgb==v) for v in [whitecol,greencol,orangecol,redcol,darkredcol]]
+      levels[key]=numcols
+      print("Read",fn)
 
 with open(cachefn,'w') as fp:
-  print("#      Location.LocalDateTime    White    Green   Orange      Red  Darkred",file=fp)
-  for name in sorted(levels):
-    print(name,' '.join("%8d"%x for x in levels[name]),file=fp)
+  print("# Location      LocalDateTime    White    Green   Orange      Red  Darkred",file=fp)
+  for (loc,date) in sorted(levels):
+    print(loc,date,' '.join("%8d"%x for x in levels[(loc,date)]),file=fp)
