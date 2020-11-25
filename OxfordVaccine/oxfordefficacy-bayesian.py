26d66997d219feabd8849426901443b3b0fd28a9
Alex Selby <alex.selby@pobox.com>
2020-11-25 05:23:01 +0000

    Fix off-by-one error

--- OxfordVaccine/oxfordefficacy-bayesian.py
+++ OxfordVaccine/oxfordefficacy-bayesian.py
@@ -16,16 +16,15 @@ for (pl,va) in [(p0,v0), (p1,v1), (p0+p1,v0+v1)]:
   # We're interested in 95% (say) point of the mu/lambda distribution
   
   N=1000000
-  Lam=gamma.rvs(pl+alpha-1, scale=1/(1+beta), size=N)
-  Mu= gamma.rvs(va+alpha-1, scale=1/(1+beta), size=N)
-  
-  l=[mu/lam for (lam,mu) in zip(Lam,Mu)]
+  Lam=gamma.rvs(pl+alpha, scale=1/(1+beta), size=N)
+  Mu= gamma.rvs(va+alpha, scale=1/(1+beta), size=N)
+  l=Mu/Lam
   l.sort()
 
   print("Number of infected participants on placebo arm:",pl)
-  print("Number of infected participants on treatment arm:",va)
+  print("Number of infected participants on vaccine arm:",va)
   print("Using prior of Gamma(shape=%f, scale=%f)"%(alpha,1/beta))
-  for c in [0.5, 0.9, 0.95, 0.98, 0.99]:
+  for c in [0.1, 0.25, 0.5, 0.9, 0.95, 0.98, 0.99]:
     print("%4.0f%% credible that efficacy >= %4.1f%%"%(c*100,(1-l[int(c*N)])*100))
   print()
-  
+
