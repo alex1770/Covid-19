7d418f592ddce10db81b975d938aee1c318d1e4f
Alex Selby <alex.selby@pobox.com>
2020-12-15 13:40:46 +0000

    Change Zoe survey case count fetching to make it more reliable

--- getzoenewcases.py
+++ getzoenewcases.py
@@ -1,6 +1,4 @@
-import requests
-import json,time
-from os.path import join
+import os,sys,requests,json,time
 
 def convdate(x):
   t=time.strptime(x,'%d %B %Y')
@@ -9,7 +7,7 @@ def convdate(x):
 tdir='zoedatapage'
 
 def updatenewcases():
-  fn=join(tdir,'zoenewcases')
+  fn=os.path.join(tdir,'zoenewcases')
   last=None
   with open(fn,'r') as fp:
     for x in fp:
@@ -22,6 +20,9 @@ def updatenewcases():
     n=int(js['uk_incidence'].replace(',',''))
     with open(fn,'a') as fp:
       print(dt,"%7d"%n,file=fp)
+    print("Wrote new cases:",n)
+    return True
+  return False
   
 if __name__=="__main__":
-  updatenewcases()
+  sys.exit(updatenewcases())
