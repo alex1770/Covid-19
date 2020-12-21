fd45ed0499120e40812e94ee15bd907c4dcbc753
Alex Selby <alex.selby@pobox.com>
2020-12-21 20:37:30 +0000

    More flexible proczoenewcases.py

--- proczoenewcases.py
+++ proczoenewcases.py
@@ -1,5 +1,6 @@
 import time,calendar,sys
 from os.path import join
+import numpy as np
 
 def datetoday(x):
   t=time.strptime(x+'UTC','%Y-%m-%d%Z')
@@ -10,24 +11,24 @@ def daytodate(r):
   return time.strftime('%Y-%m-%d',t)
 
 tdir='zoedatapage'
+name='zoenewcases'
+loc='across the UK'
+norm=''
 
-def processnewcases():
-  l=[];ok=0;offset=288
-  with open(join(tdir,'zoenewcases'),'r') as fp:
+# Returns list of (day-of-reporting, average number of new cases over 14 day period ending at day-of-reporting-4)
+def loadnewcases():
+  l=[]
+  with open(join(tdir,name),'r') as fp:
     for x in fp:
       y=x.strip().split()
-      l.append((y[0],int(y[1])))
-  
-  days=[datetoday(x[0]) for x in l]
+      l.append((datetoday(y[0]),float(y[1])))
+  days=[x[0] for x in l]
   assert days==list(range(days[0],days[-1]+1))# check contiguous
-  nn=[x[1] for x in l]
-  n=len(l)
-  period=14
-  offset=4
-  sameweight=0.1
-  
-  import numpy as np
-  # Hidden variables x[0],...,x[n+period-2], where x[period-1+i] represents the new cases at timestep i (reported on day days[i], correponding to days[i]-offset)
+  return l
+
+# Find x[0,...,n+period-2] such that nn[i] ~= x[i]+x[i+1]+...+x[i+period-1], and x[] doesn't jump too much
+def deconvolve(nn,period,sameweight):
+  n=len(nn)
   a=np.zeros((n+n+period-1,n+period-1))
   b=np.zeros(n+n+period-1)
   
@@ -43,9 +44,26 @@ def processnewcases():
   
   # Infer least squares best hidden variables
   x,resid,rank,sing=np.linalg.lstsq(a,b,rcond=-1)
+  x=np.maximum(x,0)
   
+  return x
+
+def processnewcases(pubdate):
+  l=loadnewcases()
+  n=len(l)
+  
+  days=[x[0] for x in l]
+  nn=[x[1] for x in l]
+
+  period=14
+  offset=4
+  sameweight=0.2
+  
+  # Hidden variables x[0],...,x[n+period-2], where x[period-1+i] represents the new cases on day days[i]-offset
+  x=deconvolve(nn,period,sameweight)
+
   output=[[(daytodate(days[i]-offset-period//2), "%9.1f"%nn[i]) for i in range(n)],
-          [(daytodate(days[i]-offset), "%9.1f"%x[period+i-1]) for i in range(n)]]
+          [(daytodate(days[i]-offset), "%9.1f"%x[period-1+i]) for i in range(n)]]
   
   import csv
   from subprocess import Popen,PIPE
@@ -53,7 +71,7 @@ def processnewcases():
   d0=dict(output[0])
   d1=dict(output[1])
   s=set(d0);s.update(set(d1))
-  csvfn=join(tdir,'zoenewcasesdeconvolve.csv')
+  csvfn=join(tdir,name+'deconvolve.csv')
   with open(csvfn,'w') as fp:
     writer=csv.writer(fp)
     writer.writerow(['Date','Original Total','Deconvolved'])
@@ -65,15 +83,16 @@ def processnewcases():
   # Use this to cater for earlier versions of Python whose Popen()s don't have the 'encoding' keyword
   def write(*s): p.write((' '.join(map(str,s))+'\n').encode('utf-8'))
 
-  for (size,graphfn) in [(2560,'zoenewcasesdeconvolve.png'), (1280,'zoenewcasesdeconvolve.small.png')]:
+  for (size,graphfn) in [(2560,name+'deconvolve.png'), (1280,name+'deconvolve.small.png')]:
+    lw=size//640-1
     po=Popen("gnuplot",shell=True,stdin=PIPE)
     p=po.stdin
     write('set terminal pngcairo font "sans,%d" size %d,%d'%(8+size//426,size,size//2))
     write('set bmargin 6;set lmargin 14;set rmargin %d;set tmargin 5'%(size//256-1))
     write('set output "%s"'%graphfn)
-    #write('set key left')
-    title="Zoe-estimated new cases per day across the UK from app+swab tests"
-    title+="\\nData source: https://covid.joinzoe.com/data"
+    write('set key top center')
+    title="Zoe-estimated new cases "+norm+"per day "+loc+" from app+swab tests, and soft-deconvolved version of this"
+    title+="\\nData source: https://covid.joinzoe.com/data as published "+pubdate
     write('set title "%s"'%title)
     write('set xdata time')
     write('set format x "%Y-%m-%d"')
@@ -83,10 +102,10 @@ def processnewcases():
     write('set xtics "2020-01-06", 86400*7')
     write('set xtics rotate by 45 right offset 0.5,0')
     write('set grid xtics ytics lc rgb "#dddddd" lt 1')
-    write('set ylabel "New cases per day"')
+    write('set ylabel "New cases '+norm+'per day"')
     s='plot '
-    s+='"-" using 1:2 with linespoints lw 3 title "Cases per day over %d-day period (x-axis date is centre of period)", '%period
-    s+='"-" using 1:2 with linespoints lw 3 title "Soft-deconvolved cases per day"'
+    s+=('"-" using 1:2 with linespoints lw %d title "Cases '+norm+'per day over %d-day period (x-axis date is centre of period)", ')%(lw,period)
+    s+=('"-" using 1:2 with linespoints lw %d title "Soft-deconvolved cases '+norm+'per day (end of graph less reliable)"')%lw
     write(s)
     for data in output:
       for row in data: write(row[0],row[1])
@@ -96,4 +115,7 @@ def processnewcases():
     print("Written %s"%graphfn)
 
 if __name__=="__main__":
-  processnewcases()
+  if len(sys.argv)>2: name=sys.argv[2]
+  if len(sys.argv)>3: loc=sys.argv[3]
+  if len(sys.argv)>4: norm=sys.argv[4]
+  processnewcases(sys.argv[1])
