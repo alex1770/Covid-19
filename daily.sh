d8814241daec33ecf4a8fdb6f9def19c64a7b9be
Alex Selby <alex.selby@pobox.com>
2021-01-15 09:48:40 +0000

    Moved vaccine graphs to hourly

--- daily.sh
+++ daily.sh
@@ -9,18 +9,16 @@ set -e
 make
 python3 maketrend.py
 
-python3 hosp+casesbyage.py
-
 #. rsynctraffic.sh
 
 #(cd Traffic; python3 parsetraffic.py; python3 maketrafficgraph.py; convert trafficgraph.png -resize '1200x320!' trafficgraph.small.png)
 
-bigpics='trendthr_cases.png trendthr_deaths.png trendsimple_cases.png trendsimple_deaths.png trendsimple_cases_zoom.png trendsimple_deaths_zoom.png recent_cases.png recent_deaths.png hospitaladmissionsbyage-abs.png admissionandcaseageratios.png'
+bigpics='trendthr_cases.png trendthr_deaths.png trendsimple_cases.png trendsimple_deaths.png trendsimple_cases_zoom.png trendsimple_deaths_zoom.png recent_cases.png recent_deaths.png'
 pics=$bigpics
-for x in $bigpics; do pics="$pics ${x/.png/.small.png}"; done
-
 for x in $bigpics; do
-  convert $x -resize 50% ${x/.png/.small.png}
+    small=${x/.png/.small.png}
+    pics="$pics $small"
+    convert $x -resize 50% $small
 done
 
 rsync -pt worldometer.csv $pics sonorous@sonorouschocolate.com:public_html/covid19/extdata
