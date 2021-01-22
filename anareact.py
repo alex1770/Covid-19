dae2d86403a31b1c77a8194653f83aa7051afeec
Alex Selby <alex.selby@pobox.com>
2021-01-22 16:38:33 +0000

    Add 2 parameter growth model

--- anareact.py
+++ anareact.py
@@ -3,6 +3,7 @@
 
 import os,time,calendar,csv
 from math import log,exp,sqrt
+import numpy as np
 from scipy.optimize import minimize
 
 def datetoday(x):
@@ -25,17 +26,17 @@ def load(fn,regions,mindate="0000-00-00"):
 
 ddir="reactidd/inst/extdata"
 
-# -log likelihood, with a growth parameter
-# xx=[p/(1-p), growth]
+# -log likelihood, with two growth parameters
+# xx=[p/(1-p), g1, g2]
 def LL(xx,pos,tot):
-  (lam,g)=xx# lam=p/(1-p), the odds
+  if len(xx)==2: xx=np.append(xx,0)
+  (lam0,g1,g2)=xx# lam=p/(1-p)
   n=len(pos)
-  lam*=exp(-g*(n-1)/2)# Centre so no growth factor in the middle (WLOG but may help optimisation)
-  glam=exp(g)
   s=0
-  for (np,nt) in zip(pos,tot):
-    s+=np*log(lam)-nt*log(1+lam)
-    lam*=glam
+  for i0 in range(n):
+    i=i0-(n-1)/2
+    lam=lam0*exp(g1*i+g2*i**2/2)
+    s+=pos[i0]*log(lam)-tot[i0]*log(1+lam)
   return -s
 
 # Work out Fisher information with respect to g
@@ -75,13 +76,28 @@ for (regions,desc) in [(range(1,10),"England"), ([1,7,8,9],"South England"), ([2
   print("Best constant prevalence (no growth): p=%.2f%%"%(p*100))
   print()
 
-  # Try a growth parameter
+  # Try a single growth parameter (constant growth)
   res=minimize(LL,[0.01,0],args=(pos,tot),method="SLSQP",bounds=[(1e-4,10),(-10,10)])
   if not res.success: raise RuntimeError(res.message)
-  print("Extra parameter for growth increases log likelihood by %.3f,"%(-res.fun-L))
+  DLL=-res.fun-L
+  print("Using a single parameter for growth increases log likelihood by %.3f (ΔAIC=%.1f),"%(DLL,(DLL-1)*2))
   (lam,g)=res.x
   p=lam/(1+lam)
   gerr=1/sqrt(Fisher(lam,g,pos,tot))
-  print("using p=%.2f%% on %s and growth of %.1f%%/day"%(p*100,daytodate(minday+(n-1)/2),g*100))
+  print("using p=%.2f%% on %s and growth, g, of %.1f%%/day"%(p*100,daytodate(minday+(n-1)/2),g*100))
   print("2/sqrt(Fisher information of g) = %.1f%%/day (some sort of error estimate for g)"%(2*gerr*100))
+  print()
+  
+  # Try two growth parameters
+  res=minimize(LL,[0.01,0,0],args=(pos,tot),method="SLSQP",bounds=[(1e-4,10),(-10,10),(-1,1)])
+  if not res.success: raise RuntimeError(res.message)
+  DLL=-res.fun-L
+  print("Using two parameters for growth increases log likelihood by %.3f (ΔAIC=%.1f),"%(DLL,(DLL-2)*2))
+  (lam,g1,g2)=res.x
+  p=lam/(1+lam)
+  print("using p=%.2f%% on %s and growth parameters %.1f%% %.1f%%"%(p*100,daytodate(minday+(n-1)/2),g1*100,g2*100))
+  n=len(pos)
+  print("Using these parameters, growth in prevalence would turn %s on %s"%("positive" if g2>0 else "negative",daytodate(minday+(n-1)/2-g1/g2)))
+
+  
   print("\n-----------------------------------------------\n")
