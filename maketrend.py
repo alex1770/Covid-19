161990673df9a097eb95ac63993152a8a7df92be
Alex Selby <alex.selby@pobox.com>
2021-02-08 01:04:23 +0000

    Modify country list

--- maketrend.py
+++ maketrend.py
@@ -18,7 +18,8 @@ if sys.version_info[0]<3: raise SystemExit("Error: requires Python 3")
 #selectcountries=["UK","USA","Italy","Sweden","Germany","Belgium","S. Korea","France","Switzerland","Czech Republic","Slovenia","Austria","Lithuania","Bosnia and Herzegovina"]
 #selectcountries=["UK","USA","Italy","Sweden","Germany","Belgium","S. Korea","France","Switzerland","Czech Republic","Slovenia","Austria","Lithuania","Netherlands"]
 #selectcountries=["UK","USA","Italy","Sweden","Germany","Belgium","S. Korea","France","Israel","Czech Republic","Slovenia","Ireland","Lithuania","Netherlands","South Africa"]
-selectcountries=["UK","USA","Italy","Sweden","Germany","Belgium","S. Korea","France","Israel","Czech Republic","Ireland","Netherlands","South Africa","Slovakia","Portugal"]
+#selectcountries=["UK","USA","Italy","Sweden","Germany","Belgium","S. Korea","France","Israel","Czech Republic","Ireland","Netherlands","South Africa","Slovakia","Portugal"]
+selectcountries=["UK","USA","Italy","Sweden","Germany","Spain","S. Korea","France","Israel","Czech Republic","Ireland","Netherlands","South Africa","Slovakia","Portugal"]
 
 # If perhead is True then count deaths per million population instead of absolute deaths
 perhead=True
