b1bc7517b452cb9b5871da7c70a52f99f975d0de
Alex Selby <alex.selby@pobox.com>
2021-03-12 00:37:02 +0000

    Collect LFD case data

---
+++ getlfdstats.py
@@ -0,0 +1,30 @@
+import os,json,sys
+from requests import get
+
+def get_data(req):
+  url='https://api.coronavirus.data.gov.uk/v1/data?'
+  response = get(url+req, timeout=10)
+  if not response.ok:
+    raise RuntimeError(f'Request failed: { response.text }')
+  data=response.json()['data'][::-1]
+  for d in data:
+    for x in d:
+      if d[x]==None: d[x]=0
+  return data
+
+dirname='apidata_lfd'
+
+# https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=newCasesLFDConfirmedPCRBySpecimenDate&metric=newCasesLFDOnlyBySpecimenDate&metric=newLFDTests&metric=newCasesBySpecimenDate&format=csv
+
+req='filters=areaType=nation;areaName=england&structure={"date":"date","PCRcases":"newCasesBySpecimenDate","LFDtests":"newLFDTests","LFDcasesnoPCR":"newCasesLFDOnlyBySpecimenDate","LFDcasesPCRpos":"newCasesLFDConfirmedPCRBySpecimenDate"}'
+
+data=get_data(req)
+
+updatedate=data[-1]['date']
+
+# Save case data because we might want to artificially implement cases-by-publication-date-and-age. (newCasesByPublishDateAgeDemographics not working)
+fn=os.path.join(dirname,updatedate)
+if len(sys.argv)==1 and os.path.isfile(fn): sys.exit(1)# Exit signalling no update needs to be done
+os.makedirs(dirname, exist_ok=True)
+with open(fn,'w') as fp:
+  json.dump(data,fp,indent=2)
