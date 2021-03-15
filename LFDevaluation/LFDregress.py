3b0146e303fb866db3efc73751c48bed9418286c
Alex Selby <alex.selby@pobox.com>
2021-03-15 02:06:30 +0000

    Add output suitable for graphing.

--- LFDevaluation/LFDregress.py
+++ LFDevaluation/LFDregress.py
@@ -59,8 +59,7 @@ def err(xx):
 def LL2(xx):
   ll2=0
   for (a,b,c) in zip(dd['LFDpos'],dd['LFDnum'],cases):
-    lam=(xx[0]/scale0+xx[1]*c/population)*b
-    ll2+=a/lam**2*b**2
+    ll2+=a/(xx[0]/scale0+xx[1]*c/population)**2
   return ll2
 
 best=(-1e9,)
@@ -69,16 +68,18 @@ for offset in range(-2,10):
   for date in dd["WeekEnding"]:
     day=datetoday(date)
     cases.append(sum(datetocases[daytodate(d)] for d in range(day+offset-6,day+offset+1)))
+  if 0:# Sensitivity check, to check that the correlation between LFDpos and cases is not due to LFDpos feeding directly into cases
+    for i in range(len(cases)): cases[i]-=dd['LFDpos'][i]
   
   res=minimize(err,[1,1],method="SLSQP",bounds=[(1e-9,scale0),(1e-9,100)],options={"maxiter":1000})
   if not res.success: raise RuntimeError(res.message)
   LL=-res.fun
   fisher=LL2(res.x)
   print("Offset %2d. Log likelihood = %g"%(offset,LL))
-  if LL>best[0]: best=(LL,offset,res,fisher)
+  if LL>best[0]: best=(LL,offset,res,fisher,cases)
 print()
 
-(LL,offset,res,fisher)=best
+(LL,offset,res,fisher,cases)=best
 print("Best offset %d. Log likelihood = %g"%(offset,LL))
 date=dd["WeekEnding"][-1]
 day=datetoday(date)-6
@@ -89,3 +90,7 @@ error=1.96/sqrt(fisher)*100
 print("Best estimate: LFDpos/LFDnum = %.3g + %.3g*(weekly case rate)"%(xx[0]/scale0,xx[1]))
 print("               where (weekly case rate) = (number of confirmed cases in a week) / %g"%population)
 print("False positive rate estimate: %.2g%% (%.2g%% - %.2g%%)"%(fpr,fpr-error,fpr+error))
+
+with open("graph","w") as fp:
+  for (pos,num,ncases) in zip(dd['LFDpos'],dd['LFDnum'],cases):
+    print(pos,num,ncases,1.96*sqrt(pos),file=fp)
