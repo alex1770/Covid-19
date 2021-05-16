188b70c93f441914b8900734ebf72dfde0ddc909
Alex Selby <alex.selby@pobox.com>
2021-05-16 14:56:58 +0100

    Tidy

--- VOCgrowth/growthcorrelation3.py
+++ VOCgrowth/growthcorrelation3.py
@@ -1,7 +1,7 @@
 from stuff import *
 import sys
 from scipy.optimize import minimize
-from random import randrange
+from random import randrange,seed
 
 # Get ltla_2021-05-15.csv from https://coronavirus.data.gov.uk/api/v2/data?areaType=ltla&metric=newCasesBySpecimenDate&format=csv
 
@@ -13,15 +13,21 @@ from random import randrange
 apicases=loadcsv("ltla_2021-05-15.csv")
 sgtf=loadcsv("Variants_of_Concern_Technical_Briefing_11_Data_England-1-fig13.csv")
 
-dates=['2021-04-11','2021-04-18','2021-04-25','2021-05-02','2021-05-09']
-exclude=set()# set(["E08000001"])
+# Get case counts c0, c1, c2, c3 for weeks commencing on these dates
+# Then estimate old R by (c1/c0)^(mgt/gap) and new R by (c3/c2)^(mgt/gap)
+#weeks=['2021-04-11','2021-04-18', '2021-04-25','2021-05-02']
+weeks=['2021-04-11','2021-04-18', '2021-04-28','2021-05-05']
+#weeks=['2021-04-18','2021-04-25', '2021-04-28','2021-05-05']
 mgt=5# Mean generation time in days
+exclude=set()# set(["E08000001"])
 
+days=[datetoday(w) for w in weeks]
 cases={}
 for (ltla,date,n) in zip(apicases['areaCode'],apicases['date'],apicases['newCasesBySpecimenDate']):
-  if ltla not in cases: cases[ltla]=[0]*(len(dates)-1)
-  for i in range(len(dates)-1):
-    if date>=dates[i] and date<dates[i+1]: cases[ltla][i]+=n
+  if ltla not in cases: cases[ltla]=[0,0,0,0]
+  day=datetoday(date)
+  for i in range(4):
+    if day>=days[i] and day<days[i]+7: cases[ltla][i]+=n
 
 ltlas=set()
 sgtfnum={}
@@ -37,14 +43,16 @@ with open('graph','w') as fp:
     (r,n)=sgtfnum[ltla]
     cc=cases[ltla]
     if cc[0]==0 or cc[1]==0 or cc[2]==0 or cc[3]==0: continue
-    Rold=(cc[1]/cc[0])**(mgt/7)
-    Rnew=(cc[3]/cc[2])**(mgt/7)
+    gapold=days[1]-days[0]
+    gapnew=days[3]-days[2]
+    Rold=(cc[1]/cc[0])**(mgt/gapold)
+    Rnew=(cc[3]/cc[2])**(mgt/gapnew)
     x=r/n
     y=Rnew/Rold
     print("%5.3f %5.3f"%(x,y),file=fp)
     # Calculate approx variances of x and y
     p=(r+1)/(n+2);vx=p*(1-p)/n
-    vy=y*y*sum(1/c for c in cc)*mgt/7# The mgt/7 is a fudge, but can't believe it will make too much difference
+    vy=y*y*sum(1/c for c in cc)*mgt*((1/gapold+1/gapnew)/2)# This mgt factor on the variance is a fudge
     l.append([x,y,vx,vy,n])
 
 def err(xx,l,cutoff):
@@ -57,7 +65,7 @@ def err(xx,l,cutoff):
 
 for cutoff in [1,5,10,20,30,50]:
   xx=[1,1]
-  bounds=[(0.5,1.5),(0.1,10)]
+  bounds=[(0.5,1.5),(-1,2)]
   res=minimize(err,xx,args=(l,cutoff),method="SLSQP",bounds=bounds,options={"maxiter":1000})
   if res.success:
     (a0,b0)=res.x
@@ -76,6 +84,6 @@ for cutoff in [1,5,10,20,30,50]:
     else:
       raise RuntimeError(res.message)
   sl.sort()
-  conf=0.95
+  conf=0.9917
   (bmin,bmax)=sl[int((1-conf)/2*nit)],sl[int((1+conf)/2*nit)]
   print("Cutoff %2d : %3d LTLAs : Extra transmissibility %3.0f%% (%.0f - %.0f%%)"%(cutoff,n,b0*100,bmin*100,bmax*100))
