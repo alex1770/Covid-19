b30ed5817c0e91978ad9dd1bca7b2bb7f43b9e66
Alex Selby <alex.selby@pobox.com>
2021-05-24 11:02:46 +0100

    Fetching script updates

--- Traffic/gettraffic.sh
+++ Traffic/gettraffic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 fn=London.`date -Iminutes`.png
-/usr/bin/google-chrome-stable --headless --disable-gpu --disable-features=NetworkService --screenshot=$fn --window-size=1600,1200 'https://www.google.co.uk/maps/@51.5121089,-0.1476081,11z/data=!5m1!1e1?hl=en' 2>> errors
+/usr/bin/google-chrome-stable --headless --user-data-dir=myprofile --disable-gpu --disable-features=NetworkService --screenshot=$fn --window-size=1600,1200 'https://www.google.co.uk/maps/@51.5121089,-0.1476081,11z/data=!5m1!1e1?hl=en' 2>> errors
--- daily.sh
+++ daily.sh
@@ -26,3 +26,5 @@ rsync -pt worldometer.csv $pics sonorous@sonorouschocolate.com:public_html/covid
 now=`date -Iminutes`
 google-chrome-stable --headless --disable-gpu --disable-features=NetworkService --dump-dom 'https://covid.joinzoe.com/data' > zoedatapage/data.$now 2>> zoedatapage/errors
 wget -nv https://covid-assets.joinzoe.com/latest/covid_symptom_study_report.pdf -O zoedatapage/covid_symptom_study_report.$now.pdf 2>> zoedatapage/errors
+
+wget -nv https://www.gov.uk/government/publications/covid-19-variants-genomically-confirmed-case-numbers/variants-distribution-of-cases-data -O VOC/VOCcases.$now 2>> VOC/errors
