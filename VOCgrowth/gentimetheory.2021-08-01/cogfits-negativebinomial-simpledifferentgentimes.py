4dc970b84a309d46439fec51ba4a9652d866f37f
Alex Selby <alex.selby@pobox.com>
2021-08-09 02:57:19 +0100

    COG + negative binomial + simple gentimes, different for Alpha and Delta

---
+++ VOCgrowth/gentimetheory.2021-08-01/cogfits-negativebinomial-simpledifferentgentimes.py
@@ -0,0 +1,114 @@
+from stuff import *
+import numpy as np
+from math import exp,log,sqrt
+import scipy
+from distutils.version import LooseVersion
+if LooseVersion(scipy.__version__)<LooseVersion('1.3.3'): raise RuntimeError('Need scipy>=1.3.3 to support re-entrant miminize function')
+from scipy.optimize import minimize
+from scipy.special import gammaln,digamma
+from distutils.version import LooseVersion
+
+mindate='2021-04-15'
+
+# Alpha, Delta counts by day
+A=np.zeros(1000,dtype=int)
+D=np.zeros(1000,dtype=int)
+with open('alphadelta','r') as fp:
+  maxd=0
+  for x in fp:
+    y=x.strip().split()
+    if y[0]>=mindate:
+      d=datetoday(y[0])-datetoday(mindate)
+      A[d]=int(y[2])
+      D[d]=int(y[3])
+      maxd=max(maxd,d)
+ndays=maxd+1
+
+# LLt = log( P(NB(mu,p)=a) * P(NB(e*mu^rho,p)=d) )
+def LLt(mu,a,d,e,rho,p):
+  muA=mu*(1-p)/p
+  muD=e*mu**rho*(1-p)/p
+  LLa=gammaln(a+muA)-gammaln(muA)-gammaln(a+1)+muA*log(1-p)+a*log(p)
+  LLd=gammaln(d+muD)-gammaln(muD)-gammaln(d+1)+muD*log(1-p)+d*log(p)
+  return LLa+LLd
+
+# LLt1 = d/dmu ( log( P(NB(mu,p)=a) * P(NB(e*mu^rho,p)=d) ) )
+def LLt1(mu,a,d,e,rho,p):
+  muA=mu*(1-p)/p;muA1=(1-p)/p
+  muD=e*mu**rho*(1-p)/p;muD1=e*rho*mu**(rho-1)*(1-p)/p
+  LLa1=(digamma(a+muA)-digamma(muA)+log(1-p))*muA1
+  LLd1=(digamma(d+muD)-digamma(muD)+log(1-p))*muD1
+  return LLa1+LLd1
+
+def NLLt(xx,a,d,e,rho,p):
+  return -LLt(xx[0],a,d,e,rho,p)
+
+def NLLt1(xx,a,d,e,rho,p):
+  return -LLt1(xx[0],a,d,e,rho,p)
+
+from time import process_time as clock
+tim0=0.0
+n0=0
+# rho = T_a/T_d
+# q = p/(1-p) = mu/r
+def LL(xx):
+  global n0,tim0
+  t0,lam,rho,q=xx
+  p=q/(1+q)
+  LL=0
+  for day in range(ndays):
+    e=exp(lam*(day-t0))
+    a,d=A[day],D[day]
+    if a==0 and d==0: continue
+
+    mu0=1e-5
+    mu1=2*(a*q+(d*q/e)**(1/rho))
+
+    cl0=clock()
+    # Annoyingly LLt isn't log-concave in general
+    res=minimize(NLLt,[(mu0+mu1)/2],jac=NLLt1,args=(a,d,e,rho,p),bounds=[(mu0,mu1)], method="SLSQP", options={'ftol':1e-12})
+    assert res.success
+    mu=res.x[0]
+    eps=1e-2
+    if not (LLt(mu,a,d,e,rho,p)>LLt(mu-eps,a,d,e,rho,p) and LLt(mu,a,d,e,rho,p)>LLt(mu+eps,a,d,e,rho,p)):
+      print(mu0,mu,mu1,a,d,e,rho,p)
+      print(LLt(mu-eps,a,d,e,rho,p),LLt(mu,a,d,e,rho,p),LLt(mu+eps,a,d,e,rho,p))
+      print(LLt1(mu-eps,a,d,e,rho,p),LLt1(mu,a,d,e,rho,p),LLt1(mu+eps,a,d,e,rho,p))
+    assert LLt(mu,a,d,e,rho,p)>LLt(mu-1e-3,a,d,e,rho,p) and LLt(mu,a,d,e,rho,p)>LLt(mu+1e-3,a,d,e,rho,p)
+
+    assert LLt1(mu0,a,d,e,rho,p)>0 and LLt1(mu1,a,d,e,rho,p)<0
+    while mu1-mu0>1e-12*(mu0+mu1):
+      mu=(mu0+mu1)/2
+      if LLt1(mu,a,d,e,rho,p)>0: mu0=mu
+      else: mu1=mu
+    mu=(mu0+mu1)/2
+    if abs(mu-res.x[0])>1e-3:
+      print("*",mu,res.x[0],a,d,e,rho,p)
+    
+    cl1=clock()
+    tim0+=cl1-cl0
+    n0+=1
+    if n0%1000==0:
+      print(n0,tim0/n0)
+    #print(mu-res.x[0],mu)
+    #if abs(mu-res.x[0])>1:
+    #  print(a,d,e,rho,mu,res.x)
+    #  assert 0
+    LL+=LLt(mu,a,d,e,rho,p)
+  return LL
+
+condition=1e3
+def NLL(xx): return -LL(xx)/condition
+
+def Fisher(xx,eps=1e-4):
+  t0,lam,rho,q=xx
+  fi=(-LL([t0,lam-eps,rho,q])+2*LL([t0,lam,rho,q])-LL([t0,lam+eps,rho,q]))/eps**2
+  zconf=1.96
+  return zconf/sqrt(fi)
+
+
+res=minimize(NLL,[50,0.1,1,1],bounds=[(40,60), (0,0.2), (0.5,2), (1e-4,100)], method="SLSQP")
+if not res.success: raise RuntimeError(res.message)
+t0,lam,rho,q=res.x
+dlam=Fisher(res.x)
+print("%6.2f    %5.3f (%5.3f - %5.3f)     %8.3f"%(t0,lam,lam-dlam,lam+dlam,LL(res.x)),rho,q)
