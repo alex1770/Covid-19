1cda69e088b181d3fb87cfeb60d37007341fac58
Alex Selby <alex.selby@pobox.com>
2021-09-21 22:56:27 +0100

    Get case array in the right form

--- casesbyagebleedingedge.py
+++ casesbyagebleedingedge.py
@@ -1,4 +1,4 @@
-import time,calendar,os,json,sys,datetime
+import time,calendar,os,json,sys,datetime,pytz
 from requests import get
 from subprocess import Popen,PIPE
 from math import sqrt,log,exp
@@ -11,13 +11,12 @@ np.set_printoptions(edgeitems=30, linewidth=10000)
 
 cachedir='apidata_allcaseages'
 infinity=7# Assume cases stabilise after this many days have elapsed
-
 monday=datetoday('2021-09-20')# Any Monday
 
 def get_data(req):
   url='https://api.coronavirus.data.gov.uk/v2/data?'
   for t in range(3):
-    response = requests.get(url+req, timeout=10)
+    response = requests.get(url+req, timeout=30)
     if response.ok: break
   else: raise RuntimeError('Request failed: '+response.text)
   return response.json()['body'][::-1]
@@ -29,9 +28,10 @@ def parseage(x):
   aa=[int(y) for y in x.split("_")]
   return (aa[0],aa[1]+1)
 
-minday=datetoday('2021-09-01')
+minday=datetoday('2021-06-01')
+skipdays=1
 
-d=datetime.datetime.utcnow()
+d=datetime.datetime.now(pytz.timezone("Europe/London"))
 today=datetoday(d.strftime('%Y-%m-%d'))
 if d.hour+d.minute/60<16+15/60: today-=1
 
@@ -42,11 +42,11 @@ nages=len(ages)
 # Target save format is
 # filename=publishdate, td[sex][specimendate][agerange] = cumulative cases,
 # having converted agerange to open-closed format and eliminated superfluous ranges, but kept as a string because json can't handle tuples
-# Note that specimendate goes back to the dawn of time, whatever minday is.
+# Note that specimendate goes back to the dawn of time, whatever minday is, because we want to save everything.
 # Collect dd[publishdate]=td
 dd={}
 os.makedirs(cachedir,exist_ok=True)
-for day in range(minday,today+1):
+for day in range(minday-1,today+1):
   date=daytodate(day)
   fn=os.path.join(cachedir,date)
   if os.path.isfile(fn):
@@ -71,28 +71,47 @@ for day in range(minday,today+1):
   dd[date]=td
 
 # Convert to numpy array
-# ee[publishday - minday][sex 0=m, 1=f][specimenday - (minday-infinity-1)][index into ages] = publishday's version of cumulative cases up to specimen day
+# ee[publishday - minday][sex 0=m, 1=f][specimenday - (minday-1)][index into ages] = publishday's version of cumulative cases up to specimen day
 
-# then convert from cumulative cases to new cases
-# ff[publishday - minday][sex 0=m, 1=f][specimenday - (minday-infinity)][index into ages] = publishday's version of new cases on specimen day
-
-n=today-minday
-ee=np.zeros([n+1,2,n+infinity+1,nages],dtype=int)
+npub=today-minday+1
+nspec=today-minday-skipdays
+ee=np.zeros([npub+1,2,nspec+1,nages],dtype=int)
+smindate=daytodate(minday-1)# Compare strings because datetoday is slow
 for pubdate in dd:
-  pday=datetoday(pubdate)-minday
+  pday=datetoday(pubdate)-(minday-1)
+  assert pday>=0
   for sex in dd[pubdate]:
     s=['male','female'].index(sex)
     for specdate in dd[pubdate][sex]:
-      sday=datetoday(specdate)-(minday-infinity-1)
-      if sday>=0:
-        for astring in dd[pubdate][sex][specdate]:
-          a=astrings.index(astring)
-          ee[pday][s][sday][a]=dd[pubdate][sex][specdate][astring]
-        
-ff=ee[:,:,1:,:]-ee[:,:,:-1,:]
-for i in range(n):
-  ff[i,:,infinity+i]=0
-  
-gg=ff[1:,:,:,:]-ff[:-1,:,:,:]
-hh=gg.sum(axis=(1,3))
+      if specdate>=smindate:
+        sday=datetoday(specdate)-(minday-1)
+        assert sday>=0
+        if sday<nspec+1:
+          for astring in dd[pubdate][sex][specdate]:
+            a=astrings.index(astring)
+            ee[pday][s][sday][a]=dd[pubdate][sex][specdate][astring]
 
+# Sum over sex
+# ff[publishday - (minday-1)][specimenday - (minday-1)][index into ages] = publishday's version of cumulative cases up to specimen day
+ff=ee.sum(axis=1)
+
+# Convert from cumulative cases to new cases
+# gg[publishday - (minday-1)][specimenday - minday][index into ages] = publishday's version of new cases on specimen day
+gg=ff[:,1:,:]-ff[:,:-1,:]
+for i in range(nspec): gg[i+1,i,:]=0
+
+# Convert from total up to publish day, to newly published this day
+# hh[publishday - minday][specimenday - minday][index into ages] = new cases on specimen day that were first reported on publish day
+hh=gg[1:,:,:]-gg[:-1,:,:]
+#ii=hh[:,:,-6:].sum(axis=2)
+
+# Convert into (spec day, delay) co-ords
+# jj[specimenday - minday][publishday-(specimenday+1)][index into ages] = new cases on specimen day that were first reported on publish day
+jj=np.zeros([npub-infinity,infinity,nages],dtype=int)
+for i in range(npub-infinity):
+  jj[i,:,:]=hh[i+1:i+infinity+1,i,:]
+  
+for d in range(7):
+  t=jj[d::7,:,:].sum(axis=(0,2))
+  print(t,t/t.sum()*100)
+  
