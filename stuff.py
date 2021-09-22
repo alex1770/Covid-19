35cac3784bdd0f7662fa2703b6dab2c8a0774902
Alex Selby <alex.selby@pobox.com>
2021-09-22 16:46:19 +0100

    Add makegraph

--- stuff.py
+++ stuff.py
@@ -1,4 +1,4 @@
-import csv,time,calendar,os,json,sys,datetime,requests,pytz
+import csv,time,calendar,os,json,sys,datetime,requests,pytz,subprocess
 
 # Input: 'it' is an iterable (such as a file pointer)
 # Returns map: {headings} -> [list of entries]
@@ -75,3 +75,39 @@ def api_v2(req):
 def apiday():
   now=datetime.datetime.now(tz=pytz.timezone('Europe/London'))
   return datetoday(now.strftime('%Y-%m-%d'))-(now.hour<16)
+
+def makegraph(title='A graph', data=[], mindate='0000-00-00', ylabel='', outfn='temp.png', extra=[]):
+  po=subprocess.Popen("gnuplot",shell=True,stdin=subprocess.PIPE);p=po.stdin
+  
+  # Use this to cater for earlier versions of Python whose Popen()s don't have the 'encoding' keyword
+  def write(*s): p.write((' '.join(map(str,s))+'\n').encode('utf-8'))
+
+  write('set terminal pngcairo font "sans,13" size 1920,1280')
+  write('set bmargin 5;set lmargin 15;set rmargin 15;set tmargin 5')
+  write('set output "%s"'%outfn)
+  write('set for [i=9:16] linetype i dashtype (20,7)')
+  write('set key right')
+  write('set title "%s"'%title)
+  write('set ylabel "'+ylabel+'"')
+  write('set xdata time')
+  write('set format x "%Y-%m-%d"')
+  write('set timefmt "%Y-%m-%d"')
+  write('set tics scale 2,0.5')
+  write('set xtics "2020-01-06", 604800')#%startdate)# Date labels on Mondays
+  write('set xtics rotate by 45 right offset 0.5,0')
+  write('set grid xtics ytics lc rgb "#dddddd" lt 1')
+  write('set xtics nomirror')
+  for x in extra: write(x)
+  s='plot '
+  first=True
+  for dat in data:
+    if not first: s+=', '
+    first=False
+    s+='"-" using 1:2 with lines '+dat.get('extra','')+' lw 3 title "%s"'%(dat['title'])
+  write(s)
+  for dat in data:
+    for (date,val) in dat['values']:
+      if date>=mindate: write(date,val)
+    write("e")
+  p.close();po.wait()
+  print("Written graph to %s"%outfn)
