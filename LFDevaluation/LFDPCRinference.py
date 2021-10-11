6c5a8a688c1db5e17e509e2c190ae64fc1ae08d6
Alex Selby <alex.selby@pobox.com>
2021-10-12 00:11:51 +0100

    Starting to combine with @ritwik_priya's method of determining LFD removals

--- LFDevaluation/LFDPCRinference.py
+++ LFDevaluation/LFDPCRinference.py
@@ -1,7 +1,8 @@
-import os,json
+import os,json,sys
 from scipy.special import gammaln
 from math import log
 import numpy as np
+from stuff import *
 
 np.set_printoptions(precision=3,suppress=True)
 np.set_printoptions(edgeitems=30, linewidth=100000)
@@ -14,8 +15,8 @@ np.set_printoptions(edgeitems=30, linewidth=100000)
 
 lfddir='../apidata_lfd'
 
-#mindate='2021-04-01'
-mindate='2021-08-16'
+mindate='2021-04-10'
+#mindate='2021-08-16'
 
 dates=sorted(x for x in os.listdir(lfddir) if x[:2]=='20' and x>=mindate)
 
@@ -24,7 +25,7 @@ for fn in dates:
   with open(os.path.join(lfddir,fn),'r') as fp:
     dd[fn]={y['date']: y for y in json.load(fp) if y['date']>=mindate}
 
-# dd[adjustedpublishdate][historicaldate]={historical data}
+# dd[adjustedpublishdate][historical specimen date]={historical data}
 
 now=dates[-1]
 
@@ -75,12 +76,17 @@ if 0:
     pio
 
 if 1:
-  nhist=3
+  if len(sys.argv)>1: nhist=int(sys.argv[1])
+  else: nhist=3
+  maxdays=10
   for specdate in dates[:-3]:
   #for specdate in ['2021-10-01']:
     nn=np.array([dd[repdate][specdate]['newLFDTests'] for repdate in dates if repdate>=specdate])
     oo=np.array([dd[repdate][specdate]['newCasesLFDOnlyBySpecimenDate'] for repdate in dates if repdate>=specdate])
     cc=np.array([dd[repdate][specdate]['newCasesLFDConfirmedPCRBySpecimenDate'] for repdate in dates if repdate>=specdate])
+    nn=nn[:maxdays]
+    oo=oo[:maxdays]
+    cc=cc[:maxdays]
     # Try to find (e.g., nhist=3) best a,b,c,lam s.t. a*nn+b*(0,nn[:-1])+c*(0,0,nn[:-2]) is close to oo+lam*cc
     # lam should be 1/PPV
     if specdate=='2021-10-01': nn=nn[1:];cc=cc[1:];oo=oo[1:]# Workaround presumed error in LFD test count on 2021-10-01
@@ -100,3 +106,49 @@ if 1:
       ppv=(ccn[-1]-ccn[i])/(oon[i]-oon[-1])
       print(" %5.3f"%ppv,end='')
     print(" ",rr[1],r[:nhist]*1000,r[1:nhist]/r[0]*100)
+
+def get_data(req):
+  url='https://api.coronavirus.data.gov.uk/v2/data?'
+  for t in range(3):
+    response = requests.get(url+req, timeout=30)
+    if response.ok: break
+  else: raise RuntimeError('Request failed: '+response.text)
+  return response.json()['body'][::-1]
+
+d=datetime.datetime.now(pytz.timezone("Europe/London"))
+today=datetoday(d.strftime('%Y-%m-%d'))
+if d.hour+d.minute/60<16+15/60: today-=1# Dashboard/api updates at 4pm UK time; allow 15 minutes for things to settle down
+minday=datetoday(mindate)
+
+cachedir_casesbypubdate='../apidata_casesbypubdate'
+os.makedirs(cachedir_casesbypubdate,exist_ok=True)
+todate=daytodate(today)
+fn=os.path.join(cachedir_casesbypubdate,todate)
+if os.path.isfile(fn):
+  with open(fn,'r') as fp: cases=json.load(fp)
+else:
+  cases=get_data('areaType=nation&areaCode=E92000001&metric=newCasesByPublishDate&metric=cumCasesByPublishDate&format=json')
+  with open(fn,'w') as fp: json.dump(cases,fp,indent=2)
+  print("Retrieved api data at publication date",todate)
+
+cumcasespub=np.zeros(today-minday+1,dtype=int)
+newcasespub=np.zeros(today-minday+1,dtype=int)
+for x in cases:
+  i=datetoday(x['date'])-minday
+  if i>=0:
+    cumcasespub[i]=x['cumCasesByPublishDate']
+    newcasespub[i]=x['newCasesByPublishDate']
+
+removed=newcasespub[1:]-(cumcasespub[1:]-cumcasespub[:-1])
+for i in range(today-minday-1):
+  if removed[i]==0: removed[i]=removed[i+1]//2;removed[i+1]-=removed[i]
+
+l=[]
+for day in range(minday-1,today):
+  fn=os.path.join(lfddir,daytodate(day))
+  with open(fn,'r') as fp:
+    l.append(sum(x['newCasesLFDConfirmedPCRBySpecimenDate'] for x in json.load(fp)))
+lfdconfirmed=np.array(l)
+lfdnewconfbypub=lfdconfirmed[1:]-lfdconfirmed[:-1]
+for i in range(today-minday-1):
+  if lfdnewconfbypub[i]<=0: t=lfdnewconfbypub[i]+lfdnewconfbypub[i+1];lfdnewconfbypub[i]=t//2;lfdnewconfbypub[i+1]=(t+1)//2
