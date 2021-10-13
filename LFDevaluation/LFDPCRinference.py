69141e10bf63c2f3e411915c794c67849ce6e4db
Alex Selby <alex.selby@pobox.com>
2021-10-13 16:40:04 +0100

    Changed default weight to 0

--- LFDevaluation/LFDPCRinference.py
+++ LFDevaluation/LFDPCRinference.py
@@ -77,8 +77,13 @@ if 0:
     print()
     pio
 
+# nhist = number of days of LFD history to use to predict retest results
 if len(sys.argv)>1: nhist=int(sys.argv[1])
-else: nhist=3
+else: nhist=2
+
+# weight = how strongly to force results-by-specimen-date to agree with results-by-publication-date
+if len(sys.argv)>2: weight=float(sys.argv[2])
+else: weight=0
 
 def get_data(req):
   url='https://api.coronavirus.data.gov.uk/v2/data?'
@@ -161,16 +166,16 @@ if 0:
     print(" ",rr[1],r[:nhist]*1000,r[1:nhist]/r[0]*100)
     
 if 1:
-  maxdays=10
+  stabledays=10# Number of days after which you don't expect the specimen day metrics to change
   tnnn=too=None
-  lastday=today-nhist+1
+  lastday=today-nhist
   nspec=lastday-minday
   baddays=set(i for i in range(today-minday) if removed[i]==0)
   baddays.add(datetoday('2021-10-01')-minday)
-  eqq=np.zeros([nspec-maxdays,nspec*(nhist+1)])
+  eqq=np.zeros([nspec-stabledays,nspec*(nhist+1)])
   removedadj=removed.copy()
   for i in baddays: removedadj[i+1]+=removed[i];removedadj[i]=0
-  eqo=removedadj[maxdays:nspec].copy()
+  eqo=removedadj[stabledays:nspec].copy()
   for specday in range(minday,lastday):
     # i^th element of arrays corresponds to publiction day specday+i
     specdate=daytodate(specday)
@@ -178,11 +183,11 @@ if 1:
     nn=np.array([dd[repdate][specdate]['newLFDTests'] for repdate in dates if repdate>=specdate])
     oo=np.array([dd[repdate][specdate]['newCasesLFDOnlyBySpecimenDate'] for repdate in dates if repdate>=specdate])
     cc=np.array([dd[repdate][specdate]['newCasesLFDConfirmedPCRBySpecimenDate'] for repdate in dates if repdate>=specdate])
-    drr=removed[s:][:maxdays]
-    nn=nn[:maxdays]
-    oo=oo[:maxdays]
-    cc=cc[:maxdays]
-    pp=np.arange(s,s+maxdays)# list of publication days (as an offset from minday)
+    drr=removed[s:][:stabledays]
+    nn=nn[:stabledays]
+    oo=oo[:stabledays]
+    cc=cc[:stabledays]
+    pp=np.arange(s,s+stabledays)# list of publication days (as an offset from minday)
     badrows=[i for i in range(len(drr)) if s+i in baddays]
     # Remove bad reporting days
     nn=np.delete(nn,badrows)
@@ -213,27 +218,51 @@ if 1:
     for i in range(n):
       s=specday-minday# s=spec day counted from minday
       p=pp[i]         # p=pub day counted from minday
-      if p>=maxdays and p<nspec:
-        eqq[p-maxdays,s*(nhist+1)+nhist]+=dcc[i]
-        eqo[p-maxdays]+=dcc[i]
-  weight=1
+      if p>=stabledays and p<nspec:
+        eqq[p-stabledays,s*(nhist+1)+nhist]+=dcc[i]
+        eqo[p-stabledays]+=dcc[i]
   tnnn=np.concatenate([tnnn,eqq*weight])
   too=np.concatenate([too,eqo*weight])
   trr=np.linalg.lstsq(tnnn,too)
   tr=trr[0]
   #z=eqq@tr
-  #for x in zip(dates[maxdays:-3],eqo,z): print(x)
-  for specday in range(minday,today-3):
-    specdate=daytodate(specday)
-    s=specday-minday
+  #for x in zip(dates[stabledays:-3],eqo,z): print(x)
+  removedadj2=removed.copy()
+  for i in baddays: a=(removed[i]+removed[i+1])/2;removedadj2[i]=a;removedadj2[i+1]=a
+  ccpub=np.array([sum(dd[repdate][specdate]['newCasesLFDConfirmedPCRBySpecimenDate'] for specdate in dates if repdate>=specdate) for repdate in dates])
+  dccpub=ccpub-np.concatenate([[0],ccpub])[:len(ccpub)]
+  for day in range(minday,lastday):
+    specdate=daytodate(day)
+    s=day-minday
     r=tr[s*(nhist+1):(s+1)*(nhist+1)]
     r[:nhist]=r[:nhist]/condition
     nn=np.array([dd[repdate][specdate]['newLFDTests'] for repdate in dates if repdate>=specdate])
     oo=np.array([dd[repdate][specdate]['newCasesLFDOnlyBySpecimenDate'] for repdate in dates if repdate>=specdate])
     cc=np.array([dd[repdate][specdate]['newCasesLFDConfirmedPCRBySpecimenDate'] for repdate in dates if repdate>=specdate])
-    print(specdate,"%5.3f %6.1f"%(1/r[nhist],cc[-1]*(r[nhist]-1)),end='')
+    print(specdate,"   %5.3f %6d %7.1f"%(1/r[nhist],cc[-1],cc[-1]*(r[nhist]-1)),end='')
+    if day-minday>=stabledays:
+      c=dccpub[day-minday]# Confirmed by pub date
+      d=removedadj2[day-minday]# Denied by pub date
+      print("       %5.3f %6d %6d    "%(c/(c+d),c,d),end='')
+    else:
+      print("           -      -      -    ",end='')
     ccn=cc/nn;oon=oo/nn
     for i in range(min(len(nn)-1,5)):
       ppv=(ccn[-1]-ccn[i])/(oon[i]-oon[-1])
       print(" %5.3f"%ppv,end='')
-    print(" ",trr[1],r[:nhist]*1000,r[1:nhist]/r[0]*100)
+    print(" ",r[:nhist]*1000,r[1:nhist]/r[0]*100)
+
+"""
+py LFDPCRinference.py > LFDPCRoutput
+py 7dma.py 2 3 4 5 6 7 < LFDPCRoutput > LFDPCRoutput.7dma
+gnuplot
+set xdata time;set timefmt "%Y-%m-%d";set format x "%Y-%m-%d"
+set xtics rotate by 45 right offset 0.5,0
+set terminal pngcairo font "sans,13" size 2560,1280
+set bmargin 6;set lmargin 14;set rmargin 15;set tmargin 5
+set output "LFDPCR.png"
+set xtics "2020-01-06", 604800
+set grid xtics lc rgb "#dddddd" lt 1
+set title "Estimated probability that a positive LFD test in England that is PCR-retested will be PCR positive"
+plot "LFDPCRoutput" u 1:(($3)/(($3)+($4))) w linespoints pt 5 title "PPV by specimen date", "LFDPCRoutput.7dma" u 1:(($3)/(($3)+($4))) w linespoints pt 5 title "PPV by specimen date (7 day moving average)", "LFDPCRoutput" u 1:(($6)/(($6)+($7))) w linespoints pt 5 title "PPV by publication date", "LFDPCRoutput.7dma" u 1:(($6)/(($6)+($7))) w linespoints pt 5 title "PPV by publication date (7 day moving average)"
+"""
