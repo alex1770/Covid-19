64134e1a44f43feb357cd151895d991c47b2a488
Alex Selby <alex.selby@pobox.com>
2021-11-02 13:19:42 +0000

    More thoroughly cross-checking ACGT change with COG-UK's mutation list

--- VOCgrowth/ACGTparse.py
+++ VOCgrowth/ACGTparse.py
@@ -39,7 +39,7 @@ acgt={}
 with open('aligned.examples.fasta') as fp:
   for id in fp:
     id=id[1:].strip()
-    seq=next(fp)
+    seq=next(fp).strip()
     acgt[id]=[seq,None]
 
 with open('refgenome') as fp:
@@ -49,8 +49,27 @@ me=loadcsv("mutationexamples.csv")
 for (id,ml) in zip(me['sequence_name'],me['mutations']):
   acgt[id][1]=ml.split('|')
 
+# i is nucleobase position from 0
+# Return gene name, codon number (from 0), nucleobase pos (from 0) at start of amino acid
+def getaa(i):
+  i+=1
+  for g in genes:
+    [a,b]=genes[g]
+    if i>=a and i<=b: break
+  else: return "????",1,1
+  return g,(i-a)//3,i-(i-a)%3-1
+
+# As getaa() but in orf1ab format
+def getaa2(i):
+  g,c,p=getaa(i)
+  if g=='ORF1a': g='orf1ab'
+  elif g=='ORF1b': g='orf1ab';c+=4401
+  return g,c,p
+
 for id in acgt:
   (seq,ml)=acgt[id]
+
+  # Check that all mutations on cog_metadata.csv's mutation list are what you'd expect from the ACGT sequence
   for mut in ml:
     (gene,m)=mut.split(':')
     x=m[0]
@@ -58,15 +77,30 @@ for id in acgt:
     y=m[-1]
     if gene=='synSNP':
       p=p1-1
-      continue
+      from_acgt=refgenome[p];from_mut=x
+      to_acgt=seq[p];to_mut=y
     else:
       if gene=='orf1ab':
         if p1<=4401: gene='ORF1a'
         else: gene='ORF1b';p1-=4401
       p0=genes[gene]
       p=p0[0]+(p1-1)*3-1
-      continue
-    from_acgt=codontable.get(refgenome[p:p+3],'-');from_mut=x
-    to_acgt=codontable.get(seq[p:p+3],'-');to_mut=y
-    print(from_acgt,from_mut,to_acgt,to_mut)
+      from_acgt=codontable.get(refgenome[p:p+3],'-');from_mut=x
+      to_acgt=codontable.get(seq[p:p+3],'-');to_mut=y
+    #print(from_acgt,from_mut,to_acgt,to_mut)
     assert from_acgt==from_mut and to_acgt==to_mut
+
+  # Check that all ACGT mutations are on the cog_metadata.csv's mutation list.
+  # In practice it doesn't have non-coding AAs or stuff outside genes, or ORF7b
+  n=len(refgenome)
+  sml=set(ml)
+  for i in range(n):
+    if seq[i]!='N' and seq[i]!=refgenome[i]:
+      gname,codon,p=getaa2(i)
+      x=codontable.get(refgenome[p:p+3],'?')
+      y=codontable.get(seq[p:p+3],'?')
+      if x==y:
+        mut='synSNP:%s%d%s'%(refgenome[i],i+1,seq[i])
+      else:
+        mut='%s:%s%d%s'%(gname,x,codon+1,y)
+      if mut not in sml and '?' not in mut: print("%5d %s"%(i+1,mut))
