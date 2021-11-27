cf8c9810c105f9f35e1e39dda6ff08936f547421
Alex Selby <alex.selby@pobox.com>
2021-11-27 20:34:59 +0000

    Simple inverse variance-weighted growth fit

---
+++ VOCgrowth/EarlyOmicronEstimate/simplefit.py
@@ -0,0 +1,34 @@
+# py bestfit.py < alphadelta
+# Fits cols 2, 3 (e.g., Alpha, Delta) using
+# log(Delta/Alpha) ~= a + b*day
+# Square errors are weighted by 1/(1/Alpha+1/Delta).
+
+from stuff import *
+import numpy as np
+import sys
+
+mindate='0000-00-00'
+if len(sys.argv)>1: mindate=sys.argv[1]
+
+A=[];D=[];dt=[]
+for x in sys.stdin:
+  y=x.split()
+  a,d=float(y[1]),float(y[2])
+  if y[0]>=mindate and (a>0 or d>0):
+    dt.append(y[0])
+    A.append(a)
+    D.append(d)
+
+n=len(A)
+A=np.array(A)
+D=np.array(D)
+W=A*D/(A+D)
+dt0=datetoday(dt[0])
+X=[datetoday(d)-dt0 for d in dt]
+Y=np.log((D+1e-30)/(A+1e-30))
+
+m=np.array([[sum(W), sum(W*X)], [sum(W*X), sum(W*X*X)]])
+r=np.array([sum(W*Y),sum(W*X*Y)])
+c=np.linalg.solve(m,r)
+print("Continuous growth rate = %.4f/day"%(c[1]))
+print("Crossover on",daytodate(datetoday(dt[0])+int(-c[0]/c[1]+.5)))
