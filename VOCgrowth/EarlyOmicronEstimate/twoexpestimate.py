958fbc8b2526196f2a48ad7fb4afe273488982b8
Alex Selby <alex.selby@pobox.com>
2021-12-02 12:44:56 +0000

    Added graph

--- VOCgrowth/EarlyOmicronEstimate/twoexpestimate.py
+++ VOCgrowth/EarlyOmicronEstimate/twoexpestimate.py
@@ -5,19 +5,21 @@ import numpy as np
 from random import random, seed
 
 cases=loadcsv('SAcasecounts.csv')
-prov='Total'
 
 N=len(cases['Date'])
 day0=datetoday(cases['Date'][0])
 day1=datetoday('2021-11-01')
 monday=datetoday('2021-11-01')
 np.set_printoptions(precision=4,linewidth=1000)
-
+prov='Gauteng'
 conf=0.95
 ntrials=1000
 seed(42)
-if len(sys.argv)>1: ntrials=int(sys.argv[1])
+if len(sys.argv)>1: prov=sys.argv[1]
+if len(sys.argv)>2: ntrials=int(sys.argv[2])
+locname=(prov if prov!='Total' else 'South Africa')
 
+# Model
 #         Delta                 Omicron
 # log(exp(x0+x1*(day-day1))+exp(x2+x3*(day-day1))) + x_{4+(day-monday)%7}
 # x1<0, x3>0
@@ -26,9 +28,9 @@ if len(sys.argv)>1: ntrials=int(sys.argv[1])
 # x2 ~= 2
 # x10  = 0 (gauge fix)
 
-def expand(xx):
+def expand(xx,n=N):
   l=[]
-  for d in range(N):
+  for d in range(n):
     n_delta=exp(xx[0]+xx[1]*(d+day0-day1))
     n_omicron=exp(xx[2]+xx[3]*(d+day0-day1))
     logn=log(n_delta+n_omicron)+xx[4+(day0+d-monday)%7]
@@ -91,9 +93,32 @@ params=np.array(params)
 growthadv=getconf(params[:,3]-params[:,1])
 growth=getconf(params[:,3])
 gentime=5
-print("Confidence intervals generated using a perturbation of exp(-%.3f ... %.3f) to case counts"%(perturbation,perturbation))
-print("Growth advantage of Omicron over Delta %.1f%% (%.1f%% - %.1f%%) per day"%((exp(params0[3]-params0[1])-1)*100,(exp(growthadv[0])-1)*100,(exp(growthadv[1])-1)*100))
-print("Approximate R_t(Omicron)/R_t(Delta) assuming generation time of %.1f days: %.2f (%.2f - %.2f)"%(gentime,exp((params0[3]-params0[1])*gentime),exp(growthadv[0]*gentime),exp(growthadv[1]*gentime)))
-print("Growth of Omicron %.1f%% (%.1f%% - %.1f%%) per day"%((exp(params0[3])-1)*100,(exp(growth[0])-1)*100,(exp(growth[1])-1)*100))
-print("Doubling time of Omicron %.1f (%.1f - %.1f) days"%(log(2)/params0[3],log(2)/growth[1],log(2)/growth[0]))
-print("Approximate R_t(Omicron) assuming generation time of %.1f days: %.2f (%.2f - %.2f)"%(gentime,exp(params0[3]*gentime),exp(growth[0]*gentime),exp(growth[1]*gentime)))
+text=[]
+def pr(x):
+  print(x)
+  text.append(x)
+
+#pr("Confidence intervals generated using a perturbation of exp(-%.3f ... %.3f) to case counts"%(perturbation,perturbation))
+pr("Growth advantage of Omicron over Delta: %.0f%% (%.0f%% - %.0f%%) per day"%((exp(params0[3]-params0[1])-1)*100,(exp(growthadv[0])-1)*100,(exp(growthadv[1])-1)*100))
+pr("Approximate R_t(Omicron)/R_t(Delta): %.2f (%.2f - %.2f), assuming a generation time of %.1f days"%(exp((params0[3]-params0[1])*gentime),exp(growthadv[0]*gentime),exp(growthadv[1]*gentime),gentime))
+pr("Growth of Omicron: %.0f%% (%.0f%% - %.0f%%) per day"%((exp(params0[3])-1)*100,(exp(growth[0])-1)*100,(exp(growth[1])-1)*100))
+pr("Approximate R_t(Omicron): %.2f (%.2f - %.2f), assuming a generation time of %.1f days"%(exp(params0[3]*gentime),exp(growth[0]*gentime),exp(growth[1]*gentime),gentime))
+pr("Doubling time of Omicron: %.1f (%.1f - %.1f) days"%(log(2)/params0[3],log(2)/growth[1],log(2)/growth[0]))
+pr("Program used: https://github.com/alex1770/Covid-19/blob/master/VOCgrowth/EarlyOmicronEstimate/twoexpestimate.py")
+pr("Data source: https://www.nicd.ac.za/")
+
+title='Case count in '+locname+' decomposed as sum of falling exponential for Delta + rising exponential for Omicron'
+data=[]
+data.append({
+  'title': 'Actual case count',
+  'values': [(daytodate(day0+d),log(cases[prov][d])/log(2)) for d in range(N)],
+  'with': ('points',1),
+  'extra': 'pt 5'
+})
+l=expand(params0,N)
+data.append({
+  'title': 'Model',
+  'values': [(daytodate(day0+d),l[d][3]/log(2)) for d in range(N)]
+})
+label='set label at graph 0.25,0.98 "'+'\\n'.join(text)+'"'
+makegraph(title=title, data=data, ylabel='log_2 new cases per day', outfn='SAcases.png', extra=[label,'set key left'])
