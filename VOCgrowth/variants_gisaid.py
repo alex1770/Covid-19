62e853858700955878367cb1f9f1f4e0766bc2bb
Alex Selby <alex.selby@pobox.com>
2022-01-27 07:22:26 +0000

    Added possibility for more variants

--- VOCgrowth/variants_gisaid.py
+++ VOCgrowth/variants_gisaid.py
@@ -5,14 +5,17 @@ datafile='metadata.tsv'
 cachedir='gisaidcachedir'
 
 mindate=Date('2021-11-01')
-VV=['BA.1','BA.2']
+VV=['BA.1','BA.1.1', 'BA.2']
 
 if len(sys.argv)>1: mindate=Date(sys.argv[1])
-if len(sys.argv)>2: VV[0]=sys.argv[2]
-if len(sys.argv)>3: VV[1]=sys.argv[3]
+if len(sys.argv)>2: VV=sys.argv[2].split(',')
 
+numv=len(VV)
 datamtime=datetime.datetime.utcfromtimestamp(os.path.getmtime(datafile)).strftime('%Y-%m-%d-%H-%M-%S')
-id='%s_%s_%s_%s'%(datamtime,mindate,VV[0],VV[1])
+print('Datafile: %s at %s'%(datafile,datamtime))
+print('Variants:',' '.join(VV))
+print()
+id='%s_%s_%s'%(datamtime,mindate,'_'.join(VV))
 fn=os.path.join(cachedir,id)
 if os.path.isfile(fn):
   with open(fn,'rb') as fp:
@@ -31,7 +34,7 @@ else:
     countries.add(country)
     for loc in ['World',continent,country]:
       if loc not in data: data[loc]={}
-      if date not in data[loc]: data[loc][date]=[0,0]
+      if date not in data[loc]: data[loc][date]=[0]*numv
       data[loc][date][i]+=1
   
   os.makedirs(cachedir,exist_ok=True)
@@ -39,17 +42,20 @@ else:
     pickle.dump((data,continents,countries),fp)
 
 l=list(data)
-l.sort(key=lambda x:sum(b for [a,b] in data[x].values()),reverse=True)
+l.sort(key=lambda x:sum(y[-1] for y in data[x].values()),reverse=True)
 
 n=10
 date0=Date(min(min(data[x]) for x in l[:n]))
 date1=Date(max(max(data[x]) for x in l[:n]))
 header="      Date"
-for loc in l[:n]: header+='%14s'%loc[:11]
+width=2+6*numv
+for loc in l[:n]: header+='%*s'%(width,loc[:width-3])
 print(header)
 for date in Daterange(date0,date1+1):
   print(date,end='')
   for loc in l[:n]:
-    print(' | %5d %5d'%tuple(data[loc].get(str(date),[0,0])),end='')
+    print(' |',end='')
+    for x in data[loc].get(str(date),[0]*numv):
+      print(' %5d'%x,end='')
   print()
 print(header)
