6862e56c2d2f183675d9d39d5c22e4617c369485
Alex Selby <alex.selby@pobox.com>
2022-04-09 08:22:07 +0100

    Simple GISAID processing to make count list by location and variant

---
+++ GISAID/simplecount.py
@@ -0,0 +1,52 @@
+import sys
+from stuff import *
+
+c='Europe / United Kingdom'
+mindate='2021-01-01'
+
+if len(sys.argv)>1: c=sys.argv[1]
+if len(sys.argv)>2: mindate=sys.argv[2]
+
+#VV=['B.1.1.7','B.1.617','B.1.617.1','B.1.617.2','B.1.617.3']
+#VV=['B.1.1.7','B.1.617.2','AY.1','AY.2','AY.3','AY.4','AY.5','AY.6','C.1.2']
+#VV=['B.1.617.2','AY.1','AY.2','AY.3','AY.4','AY.5','AY.6','AY.4.2']
+#VV=['B.1.617.2','AY.4','AY.4.2','AY.43']
+#VV=['B.1.617.2','AY.4','AY.4.2','AY.43','B.1.1.529']
+#VV=['B.1','B.1.2','B.1.243','B.1.1.7','B.1.1.519','B.1.427','B.1.429']
+#VV=['B.1','B.1.2','B.1.243','B.1.1.7','B.1.1.519','B.1.427','B.1.429']
+VV=['B.1.617.2','AY.*','BA.1','BA.1.1','BA.2','BA.3']
+
+print("#Country/region:",c)
+print("#From:",mindate)
+
+print('#Date             All        ',end='')
+for v in VV: print(' %10s'%v,end='')
+print('     Others')
+d={}
+for (date,loc,lineage) in csvrows('metadata.tsv',['Collection date','Location','Pango lineage'],sep='\t'):
+  if loc[:len(c)]!=c: continue
+  if len(date)==7: date+='-XX'
+  if date<mindate: continue
+  if date not in d: d[date]=[0]*(len(VV)+1)
+  if lineage in VV: i=VV.index(lineage)
+  else:
+    for i,pat in enumerate(VV):
+      if pat[-1]=='*' and lineage[:len(pat)-1]==pat[:-1]: break
+    else: i=len(VV)
+  d[date][i]+=1
+
+l=sorted(list(d))
+for date in l:
+  print(date,end='')
+  s=sum(d[date])
+  print(" %10d        "%s,end='')
+  for n in d[date]:
+    print(" %10d"%n,end='')
+  print("   ",end='')
+  #for n in d[date]:
+  #  print("  %6.1f%%"%(n/s*100),end='')
+  print()
+
+print('#Date             All        ',end='')
+for v in VV: print(' %10s'%v,end='')
+print('     Others')
