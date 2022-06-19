576eeb41795449b9a494e8db2533e86521223fa5
Alex Selby <alex.selby@pobox.com>
2022-06-19 14:39:57 +0100

    Iterative log method

--- caserateestimate/gaussianONSCARmodel.py
+++ caserateestimate/gaussianONSCARmodel.py
@@ -6,7 +6,7 @@ from scipy.optimize import minimize
 from random import normalvariate as normal,random,seed,randrange
 from parseonsprevinc import getonsprevinc
 
-startdate=Date("2021-05-31")
+startdate=Date("2022-03-01")
 if len(sys.argv)>1: startdate=Date(sys.argv[1])
 print("Start date =",startdate)
 
@@ -19,21 +19,20 @@ ignore={"2020-12-25","2020-12-26","2020-12-27","2020-12-28",
 
 # population=55.98e6
 monday=Date("2022-01-03")
-scale=1e5# Units of this number of people
+scale=1e5# Units of this number of people to keep things nicely conditioned
 
 np.set_printoptions(precision=3,suppress=True,linewidth=200)
 
 minback=1
 maxback=10
-odp=4# Overdispersion parameter. Corrects ONS variance estimates which I think are significantly too low.
-odp_casedata=1
-# Order=1 if you think the prior for xx[] is Brownian motion-like (in particular, Markov)
-# Order=2 if you think the prior for xx[] is more like the integral of Brownian motion.
-order=2
-fixediv=None
-date0_inc=startdate-maxback
-iv_inc=100
-iv_car=0.01
+# odp_ons is an overdispersion parameter. Corrects ONS variance estimates which I think are significantly too low.
+odp_ons=4         # Inverse coupling of incidence to ONS prevalence
+odp_casedata=1e-0 # Inverse coupling of incidence to case data
+iv_inc=10         # Coupling of incidence to iteself
+iv_car=10         # Coupling of inverse-CAR to itself
+# Order=1 if you think the prior is exp(Brownian motion)-like (in particular, Markov)
+# Order=2 if you think the prior is more like exp(integral of Brownian motion).
+order=1
 
 onsprev,onsinc=getonsprevinc()
 
@@ -49,7 +48,7 @@ while 1:
   data0=getcases_raw(now-completionhistory,location="England")
   if 'Bad' not in data0: break
 print("Using api data as of",now,"and comparing to",now-completionhistory)
-print("Variance multiplier for incidence and case counts =",odp)
+print("Variance multiplier for incidence and case counts =",odp_ons)
 print("Order",order)
 print()
 data0={Date(d):c for (d,c) in data0.items()}
@@ -83,6 +82,22 @@ def diffmat(n,order):
   for i in range(n-order): A[i:i+order+1,i:i+order+1]+=vv
   return A
 
+def difflogmat(n,order,xx0):
+  invxx0=1/xx0
+  logxx0=np.log(xx0)
+  A=np.zeros([n,n])
+  b=np.zeros(n)
+  c=0
+  v=[1]
+  for i in range(order): v=np.pad(v,(0,1),mode="constant")-np.pad(v,(1,0),mode="constant")
+  for i in range(n-order):
+    w=v*invxx0[i:i+order+1]
+    l=v@logxx0[i:i+order+1]
+    A[i:i+order+1,i:i+order+1]+=np.outer(w,w)
+    b[i:i+order+1]+=w*l
+    c+=l*l
+  return A,b,c
+
 
 # Positivity kernel. https://bmcmedicine.biomedcentral.com/articles/10.1186/s12916-021-01982-x, Fig 3b
 # P(PCR detection at x days after infection)
@@ -92,14 +107,34 @@ def diffmat(n,order):
 poskern=np.array([x*exp(-x/5)*0.45 for x in range(19,0,-1)])
 numk=len(poskern)
 
-# Use ONS incidence as a one-off to calibrate use of ONS prevalence, in particular
-# poskern. The ratio of (ONSincidence convolved with poskernel) to ONSprevalence varies
-# somewhat around 1, though not in an obvious pattern that depends on variants. Therefore
-# it seems most likely (or at least, no evidence against the idea) that this variation is
-# part of ONS incidence being messed up, which is a known thing, which mean we discard ONS
-# incidence for the next stage, and anchor to (calibrated) ONS prevalence.
+casedata=np.array(cases[startdate-date0_cases:])/scale
+ncases=len(casedata);assert ncases<=N
+back=[5]*7# Pro tem
 
-if 0:
+# 2N variables:
+#    i       I[i] = incidence variable 0<=i<N
+#  N+i       c[i] = CAR variable       0<=i<N
+
+def savevars(xx):
+  with open("temp2",'w') as fp:
+    for i in range(N):
+      print(startdate+i,xx[i],file=fp)
+  with open("temp3",'w') as fp:
+    for j in range(N):
+      day=(startdate+j-monday)%7
+      i=j-back[day]
+      if i>=0:
+        print(startdate+i,xx[N+j],xx[N+j]*casedata[j],file=fp)
+  
+
+def calibrateprevalencetoincidence():
+  # Use ONS incidence as a one-off to calibrate use of ONS prevalence, in particular
+  # poskern. The ratio of (ONSincidence convolved with poskernel) to ONSprevalence varies
+  # somewhat around 1, though not in an obvious pattern that depends on variants. Therefore
+  # it seems most likely (or at least, no evidence against the idea) that this variation is
+  # part of ONS incidence being messed up, which is a known thing, which mean we discard ONS
+  # incidence for the next stage, and anchor to (calibrated) ONS prevalence.
+
   iv_inc=100
   A=iv_inc*diffmat(N,order)
   b=np.zeros(N)
@@ -109,7 +144,7 @@ if 0:
   adjinc=0
   for date0,date1,targ,low,high in onsinc:
     targ/=scale
-    var=((high-low)/scale/(2*1.96))**2*odp
+    var=((high-low)/scale/(2*1.96))**2*odp_ons
     i0=date0-adjinc-startdate
     i1=date1-adjinc-startdate
     if i0>=0 and i1<=N:
@@ -122,7 +157,7 @@ if 0:
     s0=s1=s2=0
     for date0,date1,targ,low,high in onsprev:
       targ/=scale
-      var=((high-low)/scale/(2*1.96))**2*odp
+      var=((high-low)/scale/(2*1.96))**2*odp_ons
       i0=date0-numk+adjinc-startdate
       i1=date1-numk+adjinc-startdate
       if i0>=0 and i1+numk-1<=N:
@@ -137,75 +172,116 @@ if 0:
         #print(date0,pprev,targ,x)
     sd=sqrt((s2-s1**2/s0)/(s0-1))
     print(adjinc,sd)
-  qwe
 
-A=iv_inc*diffmat(N,order)
-b=np.zeros(N)
-c=0
+def initialguess():
+  
+  A=iv_inc*diffmat(N,order)
+  b=np.zeros(N)
+  c=0
 
-for date0,date1,targ,low,high in onsprev:
-  targ/=scale
-  var=((high-low)/scale/(2*1.96))**2*odp
-  i0=date0-numk-startdate
-  i1=date1-numk-startdate
-  if i0>=0 and i1+numk-1<=N:
-    #print(i0,i1,targ,sqrt(var))
-    h=i1-i0
-    poskern2=np.zeros(numk+h-1)
-    # We're assuming ONS positivity for a date range refers to the probabilty that a random person _on a random date within that range_ would test positive
-    # (As opposed to, for example, the probability that a random person would test positive on some date within that range.)
-    for i in range(h): poskern2[i:i+numk]+=poskern/h
-    A[i0:i1+numk-1,i0:i1+numk-1]+=np.outer(poskern2,poskern2)/var
-    b[i0:i1+numk-1]+=poskern2*targ/var
-    c+=targ**2/var
+  for date0,date1,targ,low,high in onsprev:
+    targ/=scale
+    var=((high-low)/scale/(2*1.96))**2*odp_ons
+    i0=date0-numk-startdate
+    i1=date1-numk-startdate
+    if i0>=0 and i1+numk-1<=N:
+      h=i1-i0
+      poskern2=np.zeros(numk+h-1)
+      # We're assuming ONS positivity for a date range refers to the probabilty that a random person _on a random date within that range_ would test positive
+      # (As opposed to, for example, the probability that a random person would test positive on some date within that range.)
+      for i in range(h): poskern2[i:i+numk]+=poskern/h
+      A[i0:i1+numk-1,i0:i1+numk-1]+=np.outer(poskern2,poskern2)/var
+      b[i0:i1+numk-1]+=poskern2*targ/var
+      c+=targ**2/var
+  
+  # Initial incidence guess
+  inc0=np.linalg.solve(A,b)
+  
+  A=iv_car*diffmat(N,order)
+  b=np.zeros(N)
+  c=0
 
-inc0=np.linalg.solve(A,b)
-with open("temp",'w') as fp:
-  for (i,x) in enumerate(inc0):
-    print(startdate+i,x,file=fp)
+  # Link cases to incidence using al[i].(I[i]-c[i'].casedata[i'])^2
+  #                               al[i]=(c[i']^2.V[casedata[i']])^{-1}
+  #                                   ~=((I[i]/casedata[i'])^2.casedata[i'].odp_casedata)^{-1}
+  #                                    =(odp_casedata.I[i']^2/casedata[i'])^{-1}
+  for j in range(ncases):
+    if startdate+j not in ignore:
+      day=(startdate+j-monday)%7
+      i=j-back[day]
+      if i>=0:
+        al=casedata[j]/(odp_casedata*inc0[i]**2)
+        A[j,j]+=al*casedata[j]**2
+        b[j]+=al*casedata[j]*inc0[i]
+        print(startdate+j,inc0[i]/casedata[j])
+  
+  # Initial inverse-CAR guess
+  c0=np.linalg.solve(A,b)
+  
+  xx=np.zeros(N*2)
+  xx[:N]=inc0
+  xx[N:]=c0
+  return xx
 
-casedata=np.array(cases[startdate-date0_cases:])/scale
-ncases=len(casedata);assert ncases<=N
-# Pad to incorporate variables c(t) rerpesenting 1/CAR(t)
-# In comments, denote i       I[i] = incidence variable 0<=i<N
-#                     N+i  c[i] = CAR variable       0<=i<ncases
-A=np.pad(A,(0,ncases),mode="constant")
-b=np.pad(b,(0,ncases),mode="constant")
+xx0=initialguess()
+#savevars(xx0)
 
-# Add in diff constraints for CAR variables which relate same days of week to each other
-# (d isn't necessarily equal to the day of the week)
-for d in range(7):
-  n=(enddate-(startdate+d)+6)//7
-  a=diffmat(n,order)
-  for i in range(n):
-    for j in range(n):
-      A[N+d+i*7,N+d+j*7]+=iv_car*a[i,j]
+for it in range(2):
+  A=np.zeros([N*2,N*2])
+  b=np.zeros(N*2)
+  c=0
 
-# Terms al[i].(I[i]-c[i'].casedata[i'])^2 correspond to CAR error at incidence i (casedata i')
-#       al[i]=V[I[i]-c[i'].casedata[i']]^{-1}
-#           ~=(odp_casedata*I0[i]^2/casedata[i'])^{-1}
-#             where I0[i] is some approximation to the incidence
-back=[5]*7# Pro tem
-al=np.zeros(N)
-for j in range(ncases):
-  if startdate+j not in ignore:
-    day=(startdate+j-monday)%7
-    i=j-back[day]
-    if i>=0:
-      al[i]=casedata[j]/(odp_casedata*inc0[i]**2)
-      A[i,i]+=al[i]
-      A[i,N+j]-=al[i]*casedata[j]
-      A[N+j,i]-=al[i]*casedata[j]
-      A[N+j,N+j]+=al[i]*casedata[j]**2
-    
+  A_i,b_i,c_i=difflogmat(N,order,xx0[:N])
+  A[:N,:N]+=iv_inc*A_i
+  b[:N]+=iv_inc*b_i
+  c+=iv_inc*c_i
+  
+  # Link incidence to ONSprevalence
+  for date0,date1,targ,low,high in onsprev:
+    targ/=scale
+    var=((high-low)/scale/(2*1.96))**2*odp_ons
+    i0=date0-numk-startdate
+    i1=date1-numk-startdate
+    if i0>=0 and i1+numk-1<=N:
+      h=i1-i0
+      poskern2=np.zeros(numk+h-1)
+      # We're assuming ONS positivity for a date range refers to the probabilty that a random person _on a random date within that range_ would test positive
+      # (As opposed to, for example, the probability that a random person would test positive on some date within that range.)
+      for i in range(h): poskern2[i:i+numk]+=poskern/h
+      A[i0:i1+numk-1,i0:i1+numk-1]+=np.outer(poskern2,poskern2)/var
+      b[i0:i1+numk-1]+=poskern2*targ/var
+      c+=targ**2/var
+  
+  
+  
+  # Add in diff constraints for CAR variables which relate same days of week to each other
+  # (d isn't necessarily equal to the day of the week)
+  for d in range(7):
+    n=(enddate-(startdate+d)+6)//7
+    A_c,b_c,c_c=difflogmat(n,order,xx0[N+d::7])
+    c+=iv_car*c_c
+    for i in range(n):
+      b[N+d+i*7]+=iv_car*b_c[i]
+      for j in range(n):
+        A[N+d+i*7,N+d+j*7]+=iv_car*A_c[i,j]
+  
+  # Terms al[i].(I[i]-c[i'].casedata[i'])^2 correspond to CAR error at incidence i (casedata i')
+  #       al[i]=(V[I[i]-c[i'].casedata[i']])^{-1}
+  #           ~=(odp_casedata.I0[i]^2/casedata[i'])^{-1}
+  #             where I0[i] is some approximation to the incidence
+  al=np.zeros(N)
+  for j in range(ncases):
+    if startdate+j not in ignore:
+      day=(startdate+j-monday)%7
+      i=j-back[day]
+      if i>=0:
+        al[i]=casedata[j]/(odp_casedata*xx0[i]**2)
+        A[i,i]+=al[i]
+        A[i,N+j]-=al[i]*casedata[j]
+        A[N+j,i]-=al[i]*casedata[j]
+        A[N+j,N+j]+=al[i]*casedata[j]**2
+  
+  xx=np.linalg.solve(A,b)
+  xx0=xx
 
-xx=np.linalg.solve(A,b)
-with open("temp2",'w') as fp:
-  for i in range(N):
-    print(startdate+i,xx[i],file=fp)
-with open("temp3",'w') as fp:
-  for j in range(N):
-    day=(startdate+j-monday)%7
-    i=j-back[day]
-    if i>=0:
-      print(startdate+i,xx[N+j],xx[N+j]*casedata[j],file=fp)
+savevars(xx)
