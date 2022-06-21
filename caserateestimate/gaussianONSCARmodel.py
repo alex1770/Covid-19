8d45eb4564284eeeb6423cf4b5f88b8565c195da
Alex Selby <alex.selby@pobox.com>
2022-06-21 19:27:02 +0100

    Add case outlier detection and tidy

--- caserateestimate/gaussianONSCARmodel.py
+++ caserateestimate/gaussianONSCARmodel.py
@@ -6,13 +6,15 @@ from scipy.optimize import minimize
 from random import normalvariate as normal,random,seed,randrange
 from parseonsprevinc import getonsprevinc
 
-startdate=Date("2021-06-01")
+startdate=Date("2020-06-01")
 if len(sys.argv)>1: startdate=Date(sys.argv[1])
 print("Start date =",startdate)
 
-ignore={"2020-12-25","2020-12-26","2020-12-27","2020-12-28",
-        "2021-12-25","2021-12-26","2021-12-27","2021-12-28",
-        "2022-12-25","2022-12-26","2022-12-27"}
+ignore={"2020-06-11", "2020-06-28", "2020-07-12", "2020-07-19", "2020-07-31", "2020-08-02", "2020-08-09", "2020-08-10", "2020-08-11", "2020-08-15",
+        "2020-08-16", "2020-08-17", "2020-08-31", "2020-09-01", "2020-09-13", "2020-09-21", "2020-11-02", "2020-12-19", "2020-12-25", "2020-12-26",
+        "2020-12-27", "2020-12-28", "2020-12-31", "2021-01-01", "2021-01-23", "2021-01-24", "2021-03-21", "2021-03-28", "2021-04-02", "2021-04-05",
+        "2021-04-18", "2021-05-09", "2021-05-31", "2021-08-30", "2021-08-31", "2021-12-25", "2021-12-29", "2022-01-01", "2022-01-04", "2022-02-18",
+        "2022-12-25", "2022-12-26", "2022-12-27"}
 
 ignore={Date(date) for date in ignore}
 
@@ -24,11 +26,10 @@ np.set_printoptions(precision=3,suppress=True,linewidth=200)
 
 minback=1
 maxback=10
-# odp_ons is an overdispersion parameter. Corrects ONS variance estimates which I think are significantly too low.
-odp_ons=4         # Inverse coupling of incidence to ONS prevalence
-odp_casedata=1    # Inverse coupling of incidence to case data
-iv_inc=10         # Coupling of incidence to iteself
-iv_car=30         # Coupling of inverse-CAR to itself
+inc_ons=1/4    # Coupling of incidence to ONS prevalence (less than 1 means we think ONS confidence intervals are too narrow)
+inc_case=1     # Coupling of incidence to case data (less than 1 means we think case data is "overdispersed" with a variance bigger than the count)
+inc_inc=30     # Coupling of incidence to iteself
+car_car=30     # Coupling of inverse-CAR to itself
 # Order=1 if you think the prior is exp(Brownian motion)-like (in particular, Markov)
 # Order=2 if you think the prior is more like exp(integral of Brownian motion).
 order=2
@@ -42,12 +43,12 @@ while 1:
   print("Can't get api data for %s. Backtracking to most recent usable date."%now)
   now-=1
 completionhistory=0
+# Change this to sampling completionhistory, to account for errors in doing this extrapolation
 while 1:
   completionhistory+=7
   data0=getcases_raw(now-completionhistory,location="England")
   if 'Bad' not in data0: break
 print("Using api data as of",now,"and comparing to",now-completionhistory)
-print("Variance multiplier for incidence and case counts =",odp_ons)
 print("Order",order)
 print()
 data0={Date(d):c for (d,c) in data0.items()}
@@ -62,7 +63,6 @@ if date0_cases>startdate: raise RuntimeError("Date %s not found in case data"%st
 cases=list(data.values())
 
 enddate=max(onsprev[-1][1],onsinc[-1][1],date0_cases+len(cases))
-
 # Possibly extend this to extrapolate
 N=enddate-startdate
 
@@ -104,6 +104,9 @@ def difflogmat(n,order,xx0):
 # I believe there isn't good evidence for the constant being different from 1.
 
 poskern=np.array([x*exp(-x/5)*0.45 for x in range(19,0,-1)])
+#poskern=np.array([10,0,0,0,0,0,0,0,0,0])
+#poskern=np.zeros(21)+0.53
+#poskern=np.zeros(50)+0.2
 numk=len(poskern)
 
 casedata=np.array(cases[startdate-date0_cases:])/scale
@@ -125,8 +128,8 @@ def savevars(xx,name="temp"):
         i=j-back[day]
         if i>=0:
           print(startdate+i,1/xx[N+j],xx[N+j]*casedata[j],file=fp)
-        
-  
+
+
 
 def calibrateprevalencetoincidence():
   # Use ONS incidence as a one-off to calibrate use of ONS prevalence, in particular
@@ -136,8 +139,8 @@ def calibrateprevalencetoincidence():
   # part of ONS incidence being messed up, which is a known thing, which mean we discard ONS
   # incidence for the next stage, and anchor to (calibrated) ONS prevalence.
 
-  iv_inc=100
-  A=iv_inc*diffmat(N,order)
+  inc_inc=100
+  A=inc_inc*diffmat(N,order)
   b=np.zeros(N)
   c=0
   # minimising quadratic form x^t.A.x-2b^t.x+c
@@ -145,7 +148,7 @@ def calibrateprevalencetoincidence():
   adjinc=0
   for date0,date1,targ,low,high in onsinc:
     targ/=scale
-    var=((high-low)/scale/(2*1.96))**2*odp_ons
+    var=((high-low)/scale/(2*1.96))**2/inc_ons
     i0=date0-adjinc-startdate
     i1=date1-adjinc-startdate
     if i0>=0 and i1<=N:
@@ -158,7 +161,7 @@ def calibrateprevalencetoincidence():
     s0=s1=s2=0
     for date0,date1,targ,low,high in onsprev:
       targ/=scale
-      var=((high-low)/scale/(2*1.96))**2*odp_ons
+      var=((high-low)/scale/(2*1.96))**2/inc_ons
       i0=date0-numk+adjinc-startdate
       i1=date1-numk+adjinc-startdate
       if i0>=0 and i1+numk-1<=N:
@@ -176,13 +179,13 @@ def calibrateprevalencetoincidence():
 
 def initialguess():
   
-  A=iv_inc*diffmat(N,order)
+  A=inc_inc*diffmat(N,order)
   b=np.zeros(N)
   c=0
   
   for date0,date1,targ,low,high in onsprev:
     targ/=scale
-    var=((high-low)/scale/(2*1.96))**2*odp_ons
+    var=((high-low)/scale/(2*1.96))**2/inc_ons
     i0=date0-numk-startdate
     i1=date1-numk-startdate
     if i0>=0 and i1+numk-1<=N:
@@ -198,20 +201,20 @@ def initialguess():
   # Initial incidence guess
   inc0=np.maximum(np.linalg.solve(A,b),0.01)
   
-  A=iv_car*diffmat(N,order)
+  A=car_car*diffmat(N,order)
   b=np.zeros(N)
   c=0
 
   # Link cases to incidence using al[i].(I[i]-c[i'].casedata[i'])^2
   #                               al[i]=(c[i']^2.V[casedata[i']])^{-1}
-  #                                   ~=((I[i]/casedata[i'])^2.casedata[i'].odp_casedata)^{-1}
-  #                                    =(odp_casedata.I[i']^2/casedata[i'])^{-1}
+  #                                   ~=((I[i]/casedata[i'])^2.(casedata[i'].inc_case))^{-1}
+  #                                    =(I[i']^2/(casedata[i'].inc_case))^{-1}
   for j in range(ncases):
     if startdate+j not in ignore:
       day=(startdate+j-monday)%7
       i=j-back[day]
       if i>=0:
-        al=casedata[j]/(odp_casedata*inc0[i]**2)
+        al=casedata[j]*inc_case/inc0[i]**2
         A[j,j]+=al*casedata[j]**2
         b[j]+=al*casedata[j]*inc0[i]
         #print(startdate+j,inc0[i]/casedata[j])
@@ -234,14 +237,14 @@ for it in range(100):
   c=0
 
   A_i,b_i,c_i=difflogmat(N,order,xx[:N])
-  A[:N,:N]+=iv_inc*A_i
-  b[:N]+=iv_inc*b_i
-  c+=iv_inc*c_i
+  A[:N,:N]+=inc_inc*A_i
+  b[:N]+=inc_inc*b_i
+  c+=inc_inc*c_i
   
   # Link incidence to ONSprevalence
   for date0,date1,targ,low,high in onsprev:
     targ/=scale
-    var=((high-low)/scale/(2*1.96))**2*odp_ons
+    var=((high-low)/scale/(2*1.96))**2/inc_ons
     i0=date0-numk-startdate
     i1=date1-numk-startdate
     if i0>=0 and i1+numk-1<=N:
@@ -255,21 +258,20 @@ for it in range(100):
       c+=targ**2/var
   
   
-  
   # Add in diff constraints for CAR variables which relate same days of week to each other
   # (d isn't necessarily equal to the day of the week)
   for d in range(7):
     n=(enddate-(startdate+d)+6)//7
     A_c,b_c,c_c=difflogmat(n,order,xx[N+d::7])
-    c+=iv_car*c_c
+    c+=car_car*c_c
     for i in range(n):
-      b[N+d+i*7]+=iv_car*b_c[i]
+      b[N+d+i*7]+=car_car*b_c[i]
       for j in range(n):
-        A[N+d+i*7,N+d+j*7]+=iv_car*A_c[i,j]
+        A[N+d+i*7,N+d+j*7]+=car_car*A_c[i,j]
   
   # Terms al[i].(I[i]-c[i'].casedata[i'])^2 correspond to CAR error at incidence i (casedata i')
   #       al[i]=(V[I[i]-c[i'].casedata[i']])^{-1}
-  #           ~=(odp_casedata.I0[i]^2/casedata[i'])^{-1}
+  #           ~=(I0[i]^2/(casedata[i'].inc_case))^{-1}
   #             where I0[i] is some approximation to the incidence
   al=np.zeros(N)
   for j in range(ncases):
@@ -277,7 +279,7 @@ for it in range(100):
       day=(startdate+j-monday)%7
       i=j-back[day]
       if i>=0:
-        al[i]=casedata[j]/(odp_casedata*xx[i]**2)
+        al[i]=casedata[j]*inc_case/xx[i]**2
         A[i,i]+=al[i]
         A[i,N+j]-=al[i]*casedata[j]
         A[N+j,i]-=al[i]*casedata[j]
@@ -288,3 +290,14 @@ for it in range(100):
   if np.abs(xx/xx0-1).max()<1e-6: break
 
 savevars(xx)
+
+#print("Case outliers not already ignored (sample days shown)")
+scd=np.log(xx[N:]*casedata)
+for j in range(1,N-1):
+  if startdate+j not in ignore:
+    l=[]
+    for k in [j-1,j+1]:
+      if startdate+k not in ignore: l.append(scd[k])
+    if len(l)>0:
+      o=scd[j]-sum(l)/len(l)
+      if abs(o)>0.2: print("Case outlier:",startdate+j,"%6.3f"%o)
