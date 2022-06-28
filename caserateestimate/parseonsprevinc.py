f0e8108c88d5138b17bb48fcfbea7eed9aad65d5
Alex Selby <alex.selby@pobox.com>
2022-06-28 02:00:07 +0100

    Extract daily prevalences from ONS spreadsheet

--- caserateestimate/parseonsprevinc.py
+++ caserateestimate/parseonsprevinc.py
@@ -1,5 +1,9 @@
 from stuff import *
+from math import sqrt
 
+# ONS Coronavirus Infection Survey data from
+# https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata
+  
 onsdir="ONSsurvey"
 
 def loadonscsv(name):
@@ -19,6 +23,25 @@ def saveonscsv(name,data):
     print("Date start,Date end,number,low,high",file=fp)
     for row in data:
       print("%s,%s,%.0f,%.0f,%.0f"%(row),file=fp)
+  print("Wrote ONS data to",fn)
+
+def loaddailycsv(name):
+  fn=os.path.join(onsdir,name+".csv")
+  if not os.path.exists(fn): return None
+  ret=[]
+  with open(fn,"r") as fp:
+    header=next(fp)
+    for x in fp:
+      l=x.strip().split(',')
+      ret.append((Date(l[0]),float(l[1]),float(l[2])))
+  return ret
+  
+def savedailycsv(name,data):
+  fn=os.path.join(onsdir,name+".csv")
+  with open(fn,"w") as fp:
+    print("Date,number,sd",file=fp)
+    for row in data:
+      print("%s,%.0f,%.0f"%(row),file=fp)
   print("Wrote ONS data to",fn)
 
 def readsheet(sheet,keys,denom):
@@ -55,19 +78,23 @@ def sheetname(xl,targ):
     if x.strip().upper()==t1: return x
   return None
 
-def getonsprevinc(maxdate="9999-12-31"):
-  import os
-  maxdate=Date(maxdate)
-  
-  # ONS Coronavirus Infection Survey data from
-  # https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/conditionsanddiseases/datasets/coronaviruscovid19infectionsurveydata
-  
+def getspreadsheetname(maxdate="9999-12-31"):
+  """
+    Returns (latestdate,latestfn)
+  """
   l=[]
   for x in os.listdir(onsdir):
     if x[-5:]==".xlsx":
       date=onsfilenamedate(x)
       if date<=maxdate: l.append((date,x))
-  (latestdate,latestfn)=max(l)
+  return max(l)
+  
+
+def getonsprevinc(maxdate="9999-12-31"):
+  import os
+  maxdate=Date(maxdate)
+  
+  (latestdate,latestfn)=getspreadsheetname(maxdate)
   
   onsprev=loadonscsv(latestdate+".prev")
   onsinc=loadonscsv(latestdate+".inc")
@@ -94,3 +121,74 @@ def getonsprevinc(maxdate="9999-12-31"):
   saveonscsv(latestdate+".inc",onsinc)
 
   return (onsprev,onsinc)
+
+def ONSconsistencycheck(mindate=Date("2022-01-01"),maxdate=Date("9999-12-31")):
+  
+  import os
+  
+  (latestdate,latestfn)=getspreadsheetname(maxdate)
+  
+  import pandas as pd
+  print("Reading ONS spreadsheet",latestfn)
+  fn=os.path.join(onsdir,latestfn)
+  xl=pd.ExcelFile(fn)
+  
+  dph=dailyprevhist=xl.parse(sheetname(xl,'1l'))
+
+  latest={}
+  earliest={}
+  for row in dph.iterrows():
+    l=list(row[1])
+    if len(l)>=4 and type(l[0])==datetime.datetime and all(type(x)==float for x in l[1:4]):
+      d=Date(l[0])
+      earliest[d]=l[1:4]
+      if d not in latest: latest[d]=l[1:4]
+
+  last=max(latest)
+  worst=(0,None)
+  s0=s2=0
+  for x in earliest:
+    if x>=mindate and x<last-28:
+      v0=earliest[x]
+      v1=latest[x]
+      sd=(v0[2]-v0[1])/(2*1.96)
+      dev=(v1[0]-v0[0])/sd
+      print(x,dev)
+      if abs(dev)>abs(worst[0]): worst=(dev,x)
+      s0+=1;s2+=dev**2
+  print()
+  print("Checked ONS daily prevalence from",mindate,"for self-consistency")
+  print("Worst deviation = %.2f standard deviations at"%worst[0],worst[1])
+  print("Correction factor of standard deviations = %.2f"%(sqrt(s2/s0)),"(i.e., narrow ONS confidence intervals by this factor)")
+
+def getdailyprevalence(maxdate=Date("9999-12-31"),correctionfactor=3.69):
+  import os
+  from itertools import chain
+
+  population=56e6
+  
+  (latestdate,latestfn)=getspreadsheetname(maxdate)
+  
+  dailyprev=loaddailycsv(latestdate+"_dailyprev")
+  if dailyprev is not None: return dailyprev
+  
+  import pandas as pd
+  print("Reading ONS spreadsheet",latestfn)
+  fn=os.path.join(onsdir,latestfn)
+  xl=pd.ExcelFile(fn)
+  
+  dpl=xl.parse(sheetname(xl,'1b'))# Latest daily prevalence
+  dph=xl.parse(sheetname(xl,'1l'))# Historic daily prevalence
+  
+  data={}
+  for row in chain(dpl.iterrows(),dph.iterrows()):
+    l=list(row[1])
+    if len(l)>=4 and type(l[0])==datetime.datetime and all(type(x)==float for x in l[1:4]):
+      d=Date(l[0])
+      if d not in data: data[d]=(l[1]/100*population,(l[3]-l[2])/(2*1.96)*correctionfactor/100*population)
+
+  dailyprev=[(d,data[d][0],data[d][1]) for d in sorted(list(data))]
+
+  savedailycsv(latestdate+"_dailyprev",dailyprev)
+  
+  return dailyprev
