bcac978e7e161fcfb44d834d40bce4ef94434fa0
Alex Selby <alex.selby@pobox.com>
2022-07-06 17:46:32 +0100

    Minor

--- COG-UK/update.sh
+++ COG-UK/update.sh
@@ -3,7 +3,7 @@
 wget https://cog-uk.s3.climb.ac.uk/phylogenetics/latest/cog_metadata.csv.gz -O temp.csv.gz
 if [[ ! -f cog_metadata.csv.gz || `diff -q temp.csv.gz cog_metadata.csv.gz` ]]; then
     mv temp.csv.gz cog_metadata.csv.gz
-    gunzip -c cog_metadata.csv.gz| python sortcog.py > cog_metadata_sorted.csv
+    gunzip -c cog_metadata.csv.gz| python3 sortcog.py > cog_metadata_sorted.csv
     #git add cog_metadata.csv 
     #git commit -m "Update" --date=`date -Iseconds --reference cog_metadata.csv`
 fi
