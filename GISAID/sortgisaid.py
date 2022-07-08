961230044975105e36fe2ddf3161bef012307894
Alex Selby <alex.selby@pobox.com>
2022-07-08 19:31:35 +0100

    Minor

--- GISAID/sortgisaid.py
+++ GISAID/sortgisaid.py
@@ -1,5 +1,8 @@
 # Sort metadata.tsv into reverse date order, and remove entries with unknown date, to make it quick to process recent entries
 # Memory requirement: approx 2.9 * (input file size) for typical GISAID file as of 2022-04-07
+#
+# Example usage:
+# tar xf metadata_tsv_2022_07_07.tar.xz metadata.csv -O | python3 sortgisaid.py > metadata_sorted.tsv
 
 import csv,sys
 
@@ -14,4 +17,3 @@ for row in reader:
   if len(row[col])==10: rest.append(row)
 rest.sort(key=lambda row: row[col], reverse=True)
 for row in rest: writer.writerow(row)
-
