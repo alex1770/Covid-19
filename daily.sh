1228081b0495845461b2176adc44dcf02951b612
Alex Selby <alex.selby@pobox.com>
2022-08-23 06:54:21 +0100

    Track new variant mixes

--- daily.sh
+++ daily.sh
@@ -16,17 +16,17 @@ set -e
     #python3 uk_var_comp.py -f 2022-05-01 -p -l 'BA.2*,BA.2.12.1,BA.4*,BA.5*'
     #python3 uk_var_comp.py -f 2022-06-01 -b -l 'BA.5.1,BA.4,BE.1,BA.5.2,BA.5.2.1'
     #python3 uk_var_comp.py -f 2022-05-01 -b -p -l 'BA.5.1,BA.2.12.1,BA.4'
-    python3 uk_var_comp.py -f 2022-06-01 -b -p -l 'BA.5.1,BA.4.6,BA.5.2,BA.5.2.1,BF.5'
-    python3 uk_var_comp.py -f 2022-06-01 -b -p -l 'BA.5*,BA.2.75'
-    python3 uk_var_comp.py -f 2022-06-01 -b -p -l 'BA.5*,BA.2.75,BA.2.76'
-    python3 uk_var_comp.py -f 2022-06-01 -b -l 'BA.5.1,BA.4.6,BA.5.2,BA.5.2.1,BF.5,BA.2*,BA.4*,BA.5*' -f1 50 -f2 50
+    #python3 uk_var_comp.py -f 2022-06-01 -b -p -l 'BA.5.1,BA.4.6,BA.5.2,BA.5.2.1,BF.5'
+    python3 uk_var_comp.py -f 2022-06-01 -b -p -l 'BA.5*,BA.2.75,BA.2.75.1'
+    python3 uk_var_comp.py -f 2022-06-25 -b -l 'BA.5*,BA.5.2+S:R346T,BA.2.75.1,BF.7' -F30
+    python3 uk_var_comp.py -f 2022-05-01 -b -l 'BA.5.1,BA.4.6,BA.5.2,BA.5.2.1,BA.5.2+S:R346T,BF.3,BF.5,BF.7,BF.11,BA.2.12.1,BA.2.75,BA.2.75.1,BA.2*,BA.4*,BA.5*' -F 50
 )
 
 make
 python3 maketrend.py
 
 bigpics='trendthr_cases.png trendthr_deaths.png trendsimple_cases.png trendsimple_deaths.png trendsimple_cases_zoom.png trendsimple_deaths_zoom.png recent_cases.png recent_deaths.png recent_cases_growth.png recent_deaths_growth.png'
-bigpics=$bigpics' VOCgrowth/UK_BA.5.1_BA.4.6_BA.5.2_BA.5.2.1_BF.5.png VOCgrowth/UK_BA.5*_BA.2.75.png VOCgrowth/UK_BA.5*_BA.2.75_BA.2.76.png VOCgrowth/UK_BA.5.1_BA.4.6_BA.5.2_BA.5.2.1_BF.5_BA.2*_BA.4*_BA.5*.png VOCgrowth/UK_BA.5.1_BA.4.6_BA.5.2_BA.5.2.1_BF.5_BA.2*_BA.4*_BA.5*.growthproj.png'
+bigpics=$bigpics' VOCgrowth/UK_BA.5*_BA.2.75_BA.2.75.1.png VOCgrowth/UK_BA.5*_BA.5.2+S:R346T_BA.2.75.1_BF.7.png VOCgrowth/UK_BA.5.1_BA.4.6_BA.5.2_BA.5.2.1_BA.5.2+S:R346T_BF.3_BF.5_BF.7_BF.11_BA.2.12.1_BA.2.75_BA.2.75.1_BA.2*_BA.4*_BA.5*.variantpressure.png'
 set -o noglob
 pics=$bigpics
 for x in $bigpics; do
@@ -34,7 +34,6 @@ for x in $bigpics; do
     pics="$pics $small"
     cat $x | convert - -resize 47% - > $small
 done
-pics="$pics VOCgrowth/UK_BA.5*_BA.2.75_BA.2.76"
 set +o noglob
 
 rsync -pt worldometer.csv $pics sonorous@sonorouschocolate.com:public_html/covid19/extdata
