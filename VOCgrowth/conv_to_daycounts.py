c2c7557bb6274d4d4f0b7b89753e9b0b19a8dc0e
Alex Selby <alex.selby@pobox.com>
2023-08-25 14:56:59 +0100

    Add GISAID / COG-UK option

--- VOCgrowth/conv_to_daycounts.py
+++ VOCgrowth/conv_to_daycounts.py
@@ -7,12 +7,20 @@ parser=argparse.ArgumentParser()
 parser.add_argument('-v', '--variant',     type=str,default="BA.2.86", help="Name of variant")
 parser.add_argument('-f', '--mindate',     default="2019-01-01",  help="Min sample date of sequence")
 parser.add_argument('-t', '--maxdate',     default="9999-12-31",  help="Max sample date of sequence")
+parser.add_argument('-g', '--gisaid',      action="store_true",   help="Use GISAID data instead of COG-UK data")
 args=parser.parse_args()
 
 from stuff import *
-fp=sys.stdin#open("gisaid_hcov-19_2023_08_22_15.tsv")
+fp=sys.stdin
+if args.gisaid:
+  keys=["Collection date","Lineage"]
+  sep="\t"
+else:
+  keys=["sample_date","lineage"]
+  sep=","
+  
 d={}
-for (date,lineage) in csvrows_it(fp,["Collection date","Lineage"],sep="\t"):
+for (date,lineage) in csvrows_it(fp,keys,sep=sep):
   if len(date)!=10 or date<args.mindate or date>args.maxdate: continue
   if date not in d: d[date]=[0,0]
   if lineage=="": continue
