6e1c5162e54f0642f37e0010df1298dc772e522b
Alex Selby <alex.selby@pobox.com>
2023-09-09 23:42:11 +0100

    Optimisation and difference threshold for sequences

--- VOCgrowth/BA.2.86_estimates/makecounts.py
+++ VOCgrowth/BA.2.86_estimates/makecounts.py
@@ -1,16 +1,22 @@
+#!/usr/bin/pypy
+
 from __future__ import print_function,division
+# ^ To enable use of pypy when pypy3 isn't available
 
 from stuff import *
-import sys,argparse
+import sys,argparse,platform
 
 reflen=29903# Length of reference genome
 targetvariant="BA.2.86"
+diffthreshold=1
 
 parser=argparse.ArgumentParser()
 parser.add_argument('-m', '--metadata',   default="metadata_sorted_from2023-01-01.tsv",  help="Metadata input file")
 parser.add_argument('-f', '--mindate',    default="2023-03-01",                          help="Minimum date selected from metadata input")
 parser.add_argument('-d', '--decluster',  action="store_true",                           help="Decluster assumes stdin is in fasta format and includes all IDs selected from metadata")
 args=parser.parse_args()
+
+if args.decluster and platform.python_implementation()=="CPython": print("Suggest using PyPy for speed\n")
 
 d={}
 for name,loc,date,lin in csvrows(args.metadata,["Virus name","Location","Collection date","Lineage"],sep="\t"):
@@ -46,17 +52,24 @@ for country in d:
 bit={"A":8, "C":4, "G":2, "T":1, "M":12, "R":10, "W":9, "S":6, "Y":5, "K":3, "V":14, "H":13, "D":11, "B":7, "N":15, "-":15}
 def declusternumber(seqs):
   nseq=0
+  l=[15]*reflen
+  for s in seqs:
+    if s=="": nseq+=1# Sequence not known, assumed isolated
+    else:
+      for i in range(reflen): l[i]&=bit[s[i]]
   sc=[]
   for s in seqs:
-    if s=="": nseq+=1;continue# Sequence not known, assumed isolated
-    sc.append([bit[x] for x in s])
+    if s!="": sc.append([bit[s[i]] for i in range(reflen) if l[i]==0])
   n=len(sc)
   deg=[0]*n
+  m=len([x for x in l if x==0])
   for i in range(n-1):
     for j in range(i+1,n):
-      same=all(sc[i][k]&sc[j][k] for k in range(reflen))
-      deg[i]+=same
-      deg[j]+=same
+      diff=0
+      for k in range(m): diff+=(sc[i][k]&sc[j][k]==0)
+      similar=(diff<=diffthreshold)
+      deg[i]+=similar
+      deg[j]+=similar
   for d in deg: nseq+=1/(d+1)
   return nseq
 
