0e636fc76e08ab5769c157a0e1a2f717e2645a2c
Alex Selby <alex.selby@pobox.com>
2023-09-10 15:42:35 +0100

    Convert COG/CLIMB metadata to sequence for declustering

--- VOCgrowth/BA.2.86_estimates/makecounts.py
+++ VOCgrowth/BA.2.86_estimates/makecounts.py
@@ -6,7 +6,7 @@ from __future__ import print_function,division
 from stuff import *
 import sys,argparse,platform,os
 
-reflen=29903# Length of reference genome
+reflen=len(refgenome)
 diffthreshold=1
 
 parser=argparse.ArgumentParser()
@@ -22,17 +22,76 @@ if args.decluster and platform.python_implementation()=="CPython": print("Sugges
 # Input is a GISAID tsv or (from UK) a CLIMB csv
 gisaidmode=(args.metadata[-4:]==".tsv")
 
+stats={}
+for geneloc in genes.values():
+  for p in range(geneloc[0],geneloc[1],3):
+    c=refgenome[p:p+3]
+    x=codontable.get(c,'?')
+    if x not in stats: stats[x]={}
+    stats[x][c]=stats[x].get(c,0)+1
+
+# Find most likely triple of bases for each AA
+# AA2bases[ new AA name, refgenome triple ] = best guess for new triple
+AA2bases={}
+w0,w1,w2=0.3,0.01,0.3# Weights that work reasonably well
+for x in stats:
+  y=stats[x]
+  best=(-1e30,)
+  s=sum(stats[x].values())
+  for old3 in codontable:
+    for new3 in y:
+      # Mutation from old3 -> new3
+      score=y[new3]/s-w0*(old3[0]!=new3[0])-w1*(old3[1]!=new3[1])-w2*(old3[2]!=new3[2])
+      if score>best[0]: best=(score,new3)
+    AA2bases[x,old3]=best[1]
+
+# Get plausible sequence based on mutation and dropout lists.
+def conv_climb_metadata_to_sequence(mutations,ambiguities):
+  ml=mutations.split('|')
+  am=ambiguities.split('|')
+  output=list(refgenome)
+  for mut in ml:
+    (gene,m)=mut.split(':')
+    x=m[0]
+    p1=int(m[1:-1])
+    y=m[-1]
+    if gene=='synSNP':
+      p=p1-1
+      output[p]=y
+    else:
+      if gene=='orf1ab':
+        if p1<=4401: gene='ORF1a'
+        else: gene='ORF1b';p1-=4401
+      p0=genes[gene]
+      p=p0[0]+(p1-1)*3-1
+      output[p:p+3]=list(AA2bases[y,refgenome[p:p+3]])
+
+  for ar in am:
+    x=[int(p)-1 for p in ar.split('-')]
+    if len(x)==1: output[x[0]]='N'
+    else: output[x[0]:x[1]+1]=['N']*(x[1]+1-x[0])
+    
+  return "".join(output)
+
 if gisaidmode:
   keys=["Virus name","Location","Collection date","Lineage"]
   sep="\t"
   source="GISAID"
 else:
-  keys=["sequence_name","country","sample_date","lineage"]
+  if args.decluster:
+    keys=["sequence_name","country","sample_date","lineage","mutations","ambiguities"]
+  else:
+    keys=["sequence_name","country","sample_date","lineage"]
   sep=","
   source="CLIMB (UK)"
 
+ind={}# Virus name -> sequence (alter name)
 d={}
-for name,loc,date,lin in csvrows(args.metadata,keys,sep=sep):
+for row in csvrows(args.metadata,keys,sep=sep):
+  if len(keys)==4:
+    name,loc,date,lin=row
+  else:
+    name,loc,date,lin,mutations,ambiguities=row
   if len(date)!=10: continue
   if date<args.mindate:
     if args.sorted: break
@@ -47,12 +106,12 @@ for name,loc,date,lin in csvrows(args.metadata,keys,sep=sep):
     country="United Kingdom"
     loc="Europe / United Kingdom / "+name.split('/')[0]
     name="hCoV-19/"+name
+    if len(keys)==6: ind[name]=conv_climb_metadata_to_sequence(mutations,ambiguities)
   if country not in d: d[country]={}
   if date not in d[country]: d[country][date]=[0,0,[]]
   d[country][date][lin[:len(args.target)]==args.target]+=1
   d[country][date][2].append((name,loc,lin))
 
-ind={}
 for country in d:
   numv=sum(x[1] for x in d[country].values())
   if numv==0: continue
@@ -62,12 +121,8 @@ for country in d:
     print("# "+source,file=fp)
     for date in sorted(list(d[country])):
       print(date,"%6d %6d"%(tuple(d[country][date][:2])),file=fp)
-  if args.decluster:
-    for date in d[country]:
-      for (name,loc,lin) in d[country][date][2]:
-        ind[name]=[]
 
-# Return the number of "sufficiently different" sequences from the set of sequences given as input
+# Return the number of "sufficiently different" sequences from the set of sequences given as input.
 # Do this by making a graph where one sequences is joined to another if it is near-identical (up to diffthreshold bases different, not counting dropout regions)
 # then sum up 1/(degree+1) over all nodes. This would be 1 if it is a single clique, and n if it is n distinct sequences, and otherwise something inbetween.
 # Bits 3210
@@ -102,21 +157,27 @@ notfound=0
 wronglength=0
 
 if args.decluster:
-  # Collect relevant sequences
-  mode=None
-  for x in sys.stdin:
-    if x[0]=='>':
-      x=x.replace(' ','_')
-      f=x.find('|')
-      if f>=0: id=x[1:f]
-      else: id=x.rstrip()[1:]
-      if id in ind: mode=ind[id]
-      else: mode=None
-    elif mode is not None: mode.append(x.rstrip())
-
-  # Concatenate and check sequences
+  if gisaidmode:
+    # Collect relevant sequences
+    for country in d:
+      for date in d[country]:
+        for (name,loc,lin) in d[country][date][2]:
+          ind[name]=[]
+    mode=None
+    for x in sys.stdin:
+      if x[0]=='>':
+        x=x.replace(' ','_')
+        f=x.find('|')
+        if f>=0: id=x[1:f]
+        else: id=x.rstrip()[1:]
+        if id in ind: mode=ind[id]
+        else: mode=None
+      elif mode is not None: mode.append(x.rstrip())
+    for id in ind:
+      ind[id]="".join(ind[id])
+  
+  # Check sequences
   for id in ind:
-    ind[id]="".join(ind[id])
     if ind[id]=="":
       notfound+=1
       #print("Sequence "+id+" not found",file=sys.stderr)
@@ -126,11 +187,12 @@ if args.decluster:
       #raise RuntimeError("Sequence %s has length %d; expecting %d for an aligned sequence"%(id,len(ind[id]),reflen))
   print(notfound,"sequence(s) not found in fasta input",file=sys.stderr)
   print(wronglength,"sequence(s) of the wrong length (not aligned)",file=sys.stderr)
-  
+    
   for country in d:
     numv=sum(x[1] for x in d[country].values())
     if numv==0: continue
-    for date in d[country]:
+    for date in sorted(list(d[country])):
+      #print("Declustering",country,date)
       subd={}
       d[country][date][:2]=[0,0]
       for (name,loc,lin) in d[country][date][2]:
@@ -138,6 +200,7 @@ if args.decluster:
         subd.setdefault((loc,isvar),[]).append(ind[name])
       for (loc,isvar) in subd:
         d[country][date][isvar]+=declusternumber(subd[loc,isvar])
+        print(date,country,loc,isvar,d[country][date][isvar])
     country_short=country.split(" / ")[-1].strip().replace(" ","_")
     with open(os.path.join("counts_decluster",country_short),"w") as fp:
       print("# "+source,file=fp)
