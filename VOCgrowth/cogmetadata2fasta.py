06f810608626a9a749a636aa0f1cdfddf2497d8e
Alex Selby <alex.selby@pobox.com>
2023-09-10 12:05:23 +0100

    Tidy

--- VOCgrowth/cogmetadata2fasta.py
+++ VOCgrowth/cogmetadata2fasta.py
@@ -10,46 +10,6 @@ parser.add_argument('-f', '--mindate',       default="2019-01-01",  help="Minimu
 parser.add_argument('-s', '--sorted',     action="store_true",      help="Whether input is sorted into reverse date order")
 args=parser.parse_args()
 
-
-# Counting from 1, inclusive-exclusive notation
-genes={
-  'ORF1a': [266, 13469],
-  'ORF1b': [13468, 21556],
-  'S': [21563, 25385],
-  'ORF3a': [25393, 26221],
-  'E': [26245, 26473],
-  'M': [26523, 27192],
-  'ORF6': [27202, 27388],
-  'ORF7a': [27394, 27760],
-  'ORF7b': [27756, 27888],
-  'ORF8': [27894, 28260],
-  'N': [28274, 29534],
-  'ORF10': [29558, 29675]
-}
-# ORF1ab codon position p>4401 <-> ORF1b codon position p-4401
-
-codontable = {
-  'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-  'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-  'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-  'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-  'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-  'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-  'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-  'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-  'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-  'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-  'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-  'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-  'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-  'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-  'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
-  'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W',
-}
-
-with open('refgenome') as fp:
-  refgenome=fp.read()
-
 stats={}
 for geneloc in genes.values():
   for p in range(geneloc[0],geneloc[1],3):
