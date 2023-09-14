d691a45cf9a6e467d058aa045ac17ce07c019b4a
Alex Selby <alex.selby@pobox.com>
2023-09-14 01:45:28 +0100

    Adjusted variant mixture

--- daily.sh
+++ daily.sh
@@ -11,17 +11,17 @@ set -e
 (cd COG-UK; bash update.sh)
 (
     cd VOCgrowth
-    python3 uk_var_comp.py -f 2023-05-01 -p -b -l 'XBB.1.16,XBB.1.16.11,EG.5.1*' -F30
-    python3 -i uk_var_comp.py -f 2023-05-01 -l 'CH.1.1.1,EG.1,EG.5.1,EG.5.1.1,FU.1,GE.1,XBB.1.16,XBB.1.16.11,XBB.2.3,XBB.2.3.2,XBB.2.3.3,*,XBB.*,XBB.1.*,XBB.1.5*,XBB.1.9.1*,XBB.1.9.2*,XBB.1.16*,BQ.1.1*,EG.*,FL.*,FE.*,FY.*' -F30
+    python3 uk_var_comp.py -f 2023-07-01 -p -b -l 'EG.5.1*,XBB.1.16*,BA.2.86*' -F30
+    python3 uk_var_comp.py -f 2023-07-01 -l 'EG.1,EG.5.1,EG.5.1.1,EG.5.1.3,EG.5.1.4,EG.5.1.6,GE.1,XBB.1.16,XBB.1.16.11,XBB.1.16.15,XBB.1.16.6,XBB.2.3,XBB.2.3.11,DV.7.1,FL.1.5.1,GK.2,*,XBB.*,XBB.1.*,XBB.1.5*,XBB.1.9.*,XBB.1.16*,EG.*,FL.*,FY.*,BA.2.86*' -F30
 )
 
 make
 python3 maketrend.py
 
 bigpics='trendthr_cases.png trendthr_deaths.png trendsimple_cases.png trendsimple_deaths.png trendsimple_cases_zoom.png trendsimple_deaths_zoom.png recent_cases.png recent_deaths.png recent_cases_growth.png recent_deaths_growth.png'
-bigpics=$bigpics' VOCgrowth/UK_XBB.1.16_XBB.1.16.11_EG.5.1*.png'
-bigpics=$bigpics' VOCgrowth/UK_CH.1.1.1_EG.1_EG.5.1_EG.5.1.1_FU.1_GE.1_XBB.1.16_XBB.1.16.11_XBB.2.3_XBB.2.3.2_XBB.2.3.3_*_XBB.*_XBB.1.*_XBB.1.5*_XBB.1.9.1*_XBB.1.9.2*_XBB.1.16*_BQ.1.1*_EG.*_FL.*_FE.*_FY.*.variantpressure.png'
-bigpics=$bigpics' VOCgrowth/UK_CH.1.1.1_EG.1_EG.5.1_EG.5.1.1_FU.1_GE.1_XBB.1.16_XBB.1.16.11_XBB.2.3_XBB.2.3.2_XBB.2.3.3_*_XBB.*_XBB.1.*_XBB.1.5*_XBB.1.9.1*_XBB.1.9.2*_XBB.1.16*_BQ.1.1*_EG.*_FL.*_FE.*_FY.*.growthproj.png'
+bigpics=$bigpics' VOCgrowth/UK_EG.5.1*_XBB.1.16*_BA.2.86*.png'
+bigpics=$bigpics' VOCgrowth/UK_EG.1_EG.5.1_EG.5.1.1_EG.5.1.3_EG.5.1.4_EG.5.1.6_GE.1_XBB.1.16_XBB.1.16.11_XBB.1.16.15_XBB.1.16.6_XBB.2.3_XBB.2.3.11_DV.7.1_FL.1.5.1_GK.2_*_XBB.*_XBB.1.*_XBB.1.5*_XBB.1.9.*_XBB.1.16*_EG.*_FL.*_FY.*_BA.2.86*.variantpressure.png'
+bigpics=$bigpics' VOCgrowth/UK_EG.1_EG.5.1_EG.5.1.1_EG.5.1.3_EG.5.1.4_EG.5.1.6_GE.1_XBB.1.16_XBB.1.16.11_XBB.1.16.15_XBB.1.16.6_XBB.2.3_XBB.2.3.11_DV.7.1_FL.1.5.1_GK.2_*_XBB.*_XBB.1.*_XBB.1.5*_XBB.1.9.*_XBB.1.16*_EG.*_FL.*_FY.*_BA.2.86*.growthproj.png'
 
 set -o noglob
 pics=$bigpics
