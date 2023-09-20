13bf47195fb67cf0292ed8daabb3752509460b74
Alex Selby <alex.selby@pobox.com>
2023-09-20 19:04:11 +0100

    Switch to fixed file naming convention

--- daily.sh
+++ daily.sh
@@ -11,17 +11,17 @@ set -e
 (cd COG-UK; bash update.sh)
 (
     cd VOCgrowth
-    python3 uk_var_comp.py -f 2023-07-01 -p -b -l 'EG.5.1*,XBB.1.16*,BA.2.86*' -F30
-    python3 uk_var_comp.py -f 2023-07-01 -l 'EG.1,EG.5.1,EG.5.1.1,EG.5.1.3,EG.5.1.4,EG.5.1.6,GE.1,XBB.1.16,XBB.1.16.11,XBB.1.16.15,XBB.1.16.6,XBB.2.3,XBB.2.3.11,DV.7.1,FL.1.5.1,GK.2,*,XBB.*,XBB.1.*,XBB.1.5*,XBB.1.9.*,XBB.1.16*,EG.*,FL.*,FY.*,BA.2.86*' -F30
+    python3 uk_var_comp.py -d -f 2023-08-15 -p -b -l 'EG.5.1*,XBB.1.16*,BA.2.86*' -F30 -n topfewvariants
+    python3 uk_var_comp.py -d -f 2023-07-01 -l 'EG.1,EG.5.1,EG.5.1.1,EG.5.1.3,EG.5.1.4,EG.5.1.6,GE.1,XBB.1.16,XBB.1.16.11,XBB.1.16.15,XBB.1.16.6,XBB.2.3,XBB.2.3.11,DV.7.1,FL.1.5.1,GK.2,*,XBB.*,XBB.1.*,XBB.1.5*,XBB.1.9.*,XBB.1.16*,EG.*,FL.*,FY.*,BA.2.86*' -F30 -n topmanyvariants
 )
 
 make
 python3 maketrend.py
 
 bigpics='trendthr_cases.png trendthr_deaths.png trendsimple_cases.png trendsimple_deaths.png trendsimple_cases_zoom.png trendsimple_deaths_zoom.png recent_cases.png recent_deaths.png recent_cases_growth.png recent_deaths_growth.png'
-bigpics=$bigpics' VOCgrowth/UK_EG.5.1*_XBB.1.16*_BA.2.86*.png'
-bigpics=$bigpics' VOCgrowth/UK_EG.1_EG.5.1_EG.5.1.1_EG.5.1.3_EG.5.1.4_EG.5.1.6_GE.1_XBB.1.16_XBB.1.16.11_XBB.1.16.15_XBB.1.16.6_XBB.2.3_XBB.2.3.11_DV.7.1_FL.1.5.1_GK.2_*_XBB.*_XBB.1.*_XBB.1.5*_XBB.1.9.*_XBB.1.16*_EG.*_FL.*_FY.*_BA.2.86*.variantpressure.png'
-bigpics=$bigpics' VOCgrowth/UK_EG.1_EG.5.1_EG.5.1.1_EG.5.1.3_EG.5.1.4_EG.5.1.6_GE.1_XBB.1.16_XBB.1.16.11_XBB.1.16.15_XBB.1.16.6_XBB.2.3_XBB.2.3.11_DV.7.1_FL.1.5.1_GK.2_*_XBB.*_XBB.1.*_XBB.1.5*_XBB.1.9.*_XBB.1.16*_EG.*_FL.*_FY.*_BA.2.86*.growthproj.png'
+bigpics=$bigpics' VOCgrowth/topfewvariants.png'
+bigpics=$bigpics' VOCgrowth/topmanyvariants.variantpressure.png'
+bigpics=$bigpics' VOCgrowth/topmanyvariants.growthproj.png'
 
 set -o noglob
 pics=$bigpics
